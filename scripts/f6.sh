176f949b8871fb93eac1dfb077e4a6ddc2321dbe
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-10 03:21:12 +0530

    Removed f6.sh

--- scripts/f6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/dash
-case "$(xdotool getactivewindow getwindowname)" in
-    *Brave | *"Google Chrome")
-        case "$1" in
-            u) xdotool key F6 ;;
-            i) xdotool key Shift+F6 ;;
-        esac
-        ;;
-esac
