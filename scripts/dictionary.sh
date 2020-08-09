3dbf161c48f7f295701d22125aa16e8f9f207f5b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-10 03:13:56 +0530

    Updated dictionary.sh

--- scripts/dictionary.sh
+++ scripts/dictionary.sh
@@ -9,7 +9,7 @@ case "$1" in
         ;;
     last)
         if [ -f /tmp/dictionary.last ] ; then
-            termite --name=dictionary_Termite -t Dictionary -e "less /tmp/dictionary.last"
+            termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         else
             notify-send -t 2000 "Dictionary" "Last word not available"
         fi
@@ -26,14 +26,14 @@ dict "$word" >/tmp/dictionary.last.temp 2>&1
 case "$?" in
     0)
         mv /tmp/dictionary.last.temp /tmp/dictionary.last
-        termite --name=dictionary_Termite -t Dictionary -e "less /tmp/dictionary.last"
+        termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         echo "$word" >>/home/ashish/Documents/.dictionary.log
         awk '!visited[$0]++' /home/ashish/Documents/.dictionary.log | tail -50 >/tmp/dictionary.log.temp
         mv /tmp/dictionary.log.temp /home/ashish/Documents/.dictionary.log
         ;;
     21)
         mv /tmp/dictionary.last.temp /tmp/dictionary.last
-        termite --name=dictionary_Termite -t Dictionary -e "less /tmp/dictionary.last"
+        termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         ;;
     *)
         rm -f /tmp/dictionary.last.temp
