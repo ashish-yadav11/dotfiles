d05dcd9f8e04dbf9084f3a28b1426e02b43ae144
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-10 02:32:30 +0530

    Use string comparison instead of arithmetic in toggletouchpad

--- scripts/toggletouchpad.sh
+++ scripts/toggletouchpad.sh
@@ -2,7 +2,7 @@
 touchpad="ETPS/2 Elantech Touchpad"
 props=$(xinput --list-props "$touchpad")
 state=${props#*Device Enabled*:	}; state=${state%%	*}
-if [ "$state" -eq 1 ] ; then
+if [ "$state" = 1 ] ; then
     xinput disable "$touchpad" && dunstify -r 24914 -t 1000 "Touchpad disabled"
 else
     xinput enable "$touchpad" && dunstify -r 24914 -t 1000 "Touchpad enabled"
