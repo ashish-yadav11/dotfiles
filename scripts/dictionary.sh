986cbc7f73a587e0b2af14bee232468ec3bdcc90
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-14 01:33:56 +0530

    Improved dictionary scripts

--- scripts/dictionary.sh
+++ scripts/dictionary.sh
@@ -28,7 +28,7 @@ case "$?" in
         mv /tmp/dictionary.last.temp /tmp/dictionary.last
         termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         echo "$word" >>/home/ashish/Documents/.dictionary.log
-        awk '!visited[$0]++' /home/ashish/Documents/.dictionary.log | tail -50 >/tmp/dictionary.log.temp
+        awk '!visited[$0]++' /home/ashish/Documents/.dictionary.log | tail -10000 >/tmp/dictionary.log.temp
         mv /tmp/dictionary.log.temp /home/ashish/Documents/.dictionary.log
         ;;
     21)
--- scripts/dictionary_history.sh
+++ scripts/dictionary_history.sh
@@ -2,7 +2,11 @@
 log=$(tac /home/ashish/Documents/.dictionary.log)
 if [ -n "$log" ] ; then
     word=$(echo "$log" | fzf --sync)
-    [ -n "$word" ] && dict "$word" | less
+    [ -n "$word" ] && dict "$word" >/tmp/dictionary.last
+    termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
+    echo "$word" >>/home/ashish/Documents/.dictionary.log
+    awk '!visited[$0]++' /home/ashish/Documents/.dictionary.log | tail -10000 >/tmp/dictionary.log.temp
+    mv /tmp/dictionary.log.temp /home/ashish/Documents/.dictionary.log
 else
     notify-send -t 2000 "Dictionary" "History log is empty"
 fi
