bc5da3625ed5d84c9d658bae72db92a64edf0b44
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-14 01:51:12 +0530

    Fixed a bug in dictionary_history.sh

--- scripts/dictionary_history.sh
+++ scripts/dictionary_history.sh
@@ -3,7 +3,7 @@ log=$(tac /home/ashish/Documents/.dictionary.log)
 if [ -n "$log" ] ; then
     word=$(echo "$log" | fzf --sync)
     [ -n "$word" ] && dict "$word" >/tmp/dictionary.last
-    termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
+    less /tmp/dictionary.last
     echo "$word" >>/home/ashish/Documents/.dictionary.log
     awk '!visited[$0]++' /home/ashish/Documents/.dictionary.log | tail -10000 >/tmp/dictionary.log.temp
     mv /tmp/dictionary.log.temp /home/ashish/Documents/.dictionary.log
