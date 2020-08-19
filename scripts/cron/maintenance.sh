e1c5774fe12e8ca5bc5829a5b6f7b3432807646d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-20 03:09:54 +0530

    Added reminder to reset atq in maintenance.sh

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -4,7 +4,7 @@ export DISPLAY=":0"
 /home/ashish/.local/bin/sigdsblocks 10 # update time in status
 date=$(date +%d)
 if [ "$date" -le 07 ] || { [ "$date" -ge 15 ] && [ "$date" -le 21 ] ;} ; then
-    notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Timeshift log, Cache of uninstalled packages\n3) Backup"
+    notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Timeshift log, Cache of uninstalled packages\n3) Reset atq\n4) Backup"
 else
     notify-send -t 0 "Upgrade system"
 fi
