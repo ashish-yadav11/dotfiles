0bd059cfbab1dac920582e1ebc1d8e2d8b1f9845
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-20 19:29:08 +0530

    Improved toggletouchpad.sh

--- scripts/toggletouchpad.sh
+++ scripts/toggletouchpad.sh
@@ -1,8 +1,6 @@
 #!/bin/dash
 touchpad="ETPS/2 Elantech Touchpad"
-props=$(xinput --list-props "$touchpad")
-state=${props#*Device Enabled*:	}; state=${state%%	*}
-if [ "$state" = 1 ] ; then
+if xinput --list-props "$touchpad" | grep -q "Device Enabled.*1$" ; then
     xinput disable "$touchpad" && dunstify -r 24914 -t 1000 "Touchpad disabled"
 else
     xinput enable "$touchpad" && dunstify -r 24914 -t 1000 "Touchpad enabled"
