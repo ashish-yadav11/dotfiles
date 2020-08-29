951e139da642305eeb5be07276638aedf84d170f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-30 03:07:24 +0530

    Deduplicate calcurse appointments after import - mutt

---
+++ configs/mutt/scripts/import_in_calcurse.sh
@@ -0,0 +1,7 @@
+#!/bin/dash
+aptsfile=/home/ashish/.local/share/calcurse/apts
+
+calcurse -i "$1"
+uniq "$aptsfile" /tmp/apts.temp
+mv /tmp/apts.temp "$aptsfile"
+notify-send -t 1000 "Calcurse" "Import successful"
