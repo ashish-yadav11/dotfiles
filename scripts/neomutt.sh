46927b40eb1fa268b85f5fb03b3570c5756a29d0
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-30 02:02:39 +0530

    Improved neomutt.sh

--- scripts/neomutt.sh
+++ scripts/neomutt.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
-termite --name=neomutt_Termite -e "bash -c 'stty -ixon; neomutt'"
-pidof -s /usr/bin/neomutt || rm -rf /tmp/neomutt/
+termite --name=neomutt_Termite -e "dash -c 'stty -ixon; neomutt'"
+pidof -s /usr/bin/neomutt >/dev/null 2>&1 || rm -rf /tmp/neomutt/
