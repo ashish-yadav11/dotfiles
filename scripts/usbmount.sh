2b2bafeab3e19cfb7eb194c374779abe8e1eea0b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-09-02 00:41:15 +0530

    Improved usbmount.sh

--- scripts/usbmount.sh
+++ scripts/usbmount.sh
@@ -7,16 +7,24 @@ drives_to_mount=$(lsblk -nrpo "name,type,mountpoint,label,size" | awk -F'[ ]' '{
 drives_to_unmount=$(lsblk -nrpo "name,type,mountpoint,label,size" | awk -F'[ ]' '{if ($2=="part" && $3!="" && $3!="/" && $3!="/efi" && $3!="[SWAP]" && $3!="/media/backup" && $3!="/media/storage") {if ($4!="") {printf "%s (%s-%s)\n",$1,$4,$5} else {printf "%s (%s)\n",$1,$5}}}')
 
 usbmount() {
-    echo "$drives_to_mount" | $dmenu_command -p "Which drive(s) to mount?" | cut -d" " -f1 |
+    echo "$drives_to_mount" | $dmenu_command -p "Which drive(s) to mount?" |
         while read -r chosen ; do
-            notify-send -t 2000 " USB mounter" "$(udisksctl mount -b "$chosen")"
+            if output=$(udisksctl mount -b "${chosen%% *}") ; then
+                notify-send -t 2000 " USB mounter" ""
+            else
+                notify-send -u critical " USB mounter" "Error mounting $chosen"
+            fi
         done
 }
 
 usbunmount() {
-    echo "$drives_to_unmount" | $dmenu_command -p "Which drive(s) to unmount?" | cut -d" " -f1 |
+    echo "$drives_to_unmount" | $dmenu_command -p "Which drive(s) to unmount?" |
         while read -r chosen ; do
-            notify-send -t 2000 " USB mounter" "$(udisksctl unmount -b "$chosen")"
+            if output=$(udisksctl unmount -b "${chosen%% *}") ; then
+                notify-send -t 2000 " USB mounter" "$output"
+            else
+                notify-send -u critical " USB mounter" "Error unmounting $chosen"
+            fi
         done
 }
 
@@ -24,16 +32,24 @@ asktype() {
     M=$(echo "$drives_to_mount" | awk 'BEGIN {ORS=""} {if (!x) {x=1; print $0} else {print ", "$0}}')
     U=$(echo "$drives_to_unmount" | awk 'BEGIN {ORS=""} {if (!x) {x=1; print $0} else {print ", "$0}}')
     case "$(echo "Mount: $M\nUnmount: $U" | dmenu -i -matching fuzzy -no-custom -p "What do you want me to do?")" in
-        Mount*) 
+        Mount*)
             if [ "$(echo "$drives_to_mount" | wc -l)" = 1 ] ; then
-                notify-send -t 2000 " USB mounter" "$(udisksctl mount -b "${drives_to_mount%% *}")"
+                if output=$(udisksctl mount -b "${drives_to_mount%% *}") ; then
+                    notify-send -t 2000 " USB mounter" "$output"
+                else
+                    notify-send -u critical " USB mounter" "Error mounting $drives_to_mount"
+                fi
             else
                 usbmount
             fi
         ;;
 	Unmount*)
             if [ "$(echo "$drives_to_unmount" | wc -l)" = 1 ] ; then
-                notify-send -t 2000 " USB mounter" "$(udisksctl unmount -b "${drives_to_unmount%% *}")"
+                if output=$(udisksctl unmount -b "${drives_to_unmount%% *}") ; then
+                    notify-send -t 2000 " USB mounter" "$output"
+                else
+                    notify-send -u critical " USB mounter" "Error unmounting $drives_to_unmount"
+                fi
             else
                 usbunmount
             fi
