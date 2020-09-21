9b092ba88a4b3cafd234598934503a367441bb65
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-09-21 22:04:20 +0530

    Improved sxiv shell scripts

--- configs/sxiv/exec/wallpaper.sh
+++ configs/sxiv/exec/wallpaper.sh
@@ -2,4 +2,4 @@
 feh --bg-scale "$1"
 rm -f /home/ashish/.config/wall*
 filename=$(basename "$1")
-cp "$1" /home/ashish/.config/wall."${filename##*.}"
+cp "$1" "/home/ashish/.config/wall.${filename##*.}"
