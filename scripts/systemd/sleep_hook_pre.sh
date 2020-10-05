1f08a797d3aaed463b28c8cd91219662bb87de42
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-10-06 00:08:14 +0530

    Improved sleep_hook_pre.sh

--- scripts/systemd/sleep_hook_pre.sh
+++ scripts/systemd/sleep_hook_pre.sh
@@ -1,3 +1,4 @@
 #!/bin/dash
-read -r PID 2>/dev/null </tmp/sleep_dine.pid && rkill "$PID"
+exec >/dev/null 2>&1
+read -r PID </tmp/sleep_dine.pid && rkill "$PID"
 sudo /home/ashish/.scripts/hotspot.sh terminate
