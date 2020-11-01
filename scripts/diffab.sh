57af1fee241462325b563cd72d542012b73bcb1b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-01 23:09:58 +0530

    Improved diffab.sh

--- scripts/diffab.sh
+++ scripts/diffab.sh
@@ -1,73 +1,70 @@
 #!/bin/bash
-diff_cmd="diff --color=always -r"
+mdiff_cmd="diff --color=always -r"
+sdiff_cmd="diff --color=always"
+sdiff_str="diff '--color=always'"
 dsblocks=/home/ashish/.local/projects/dsblocks
 dotfiles=/home/ashish/.local/dotfiles
 
-print() {
-    if [[ -n $diff ]] ; then
-        printf "%s\n\n" "$diff"
+print_mdiff() {
+    if [[ -n $mdiff ]] ; then
+        printf "%s\n\n" "$mdiff"
     else
         echo -e "No changes\n"
     fi
 }
 
 echo -e "\e[1;32mbackup\e[0m"
-diff=$(
-    $diff_cmd /media/storage/.backup/ /home/ashish/ |
+mdiff=$(
+    $mdiff_cmd /media/storage/.backup/ /home/ashish/ |
         grep -Ev '^Only.*'
-    $diff_cmd /media/storage/.backup/ /home/ashish/.config/ |
+    $mdiff_cmd -I '^token = ' /media/storage/.backup/ /home/ashish/.config/ |
         grep -Ev '^Only.*'
 )
-print
+print_mdiff
 
 echo -e "\e[1;32mdsblocks\e[0m"
-diff=$(
-    $diff_cmd "$dsblocks/helpers/scripts/" /home/ashish/.scripts/ |
+mdiff=$(
+    $mdiff_cmd "$dsblocks/helpers/scripts/" /home/ashish/.scripts/ |
         grep -Ev '^Only.*'
 )
-print
+print_mdiff
 
 echo -e "\e[1;32mdotfiles\e[0m"
-diff=$(
-    diff1=$(
-        $diff_cmd "$dotfiles/configs/bash.bashrc" /etc/bash.bashrc
+mdiff=$(
+    sdiff=$(
+        $sdiff_cmd "$dotfiles/configs/bash.bashrc" /etc/bash.bashrc
     )
-    if [[ -n $diff1 ]] ; then
-        echo "diff '--color=always' -r "$dotfiles/configs/bash.bashrc" /etc/bash.bashrc"
-        printf "%s\n" "$diff1"
-    fi
-    $diff_cmd "$dotfiles/configs/" /home/ashish/.config/ |
+    [[ -n $sdiff ]] &&
+        printf "$sdiff_str $dotfiles/configs/bash.bashrc /etc/bash.bashrc\n%s\n" "$sdiff"
+
+    sdiff=$(
+        $sdiff_cmd "$dotfiles/configs/crontab" <( crontab -l )
+    )
+    [[ -n $sdiff ]] &&
+        printf "$sdiff_str $dotfiles/configs/crontab crontab\n%s\n" "$sdiff"
+
+    $mdiff_cmd -I "^history\|^lastVisited\|^':" "$dotfiles/configs/" /home/ashish/.config/ |
         grep -Ev '^Only.*(configs?/:)|(nvim: \.netrwhist)|(ranger: tagged)|(newsboat: cache.db)|(msmtp: msmtp.log)|(mpv: watch_later)'
-    $diff_cmd "$dotfiles/configs/" /home/ashish/ |
+    $mdiff_cmd "$dotfiles/configs/" /home/ashish/ |
         grep -Ev '^Only.*(configs/:)|(ashish/(:)|(.surf:))|(GmailAPI: token)|(\.gnupg: )'
-    $diff_cmd "$dotfiles/locals/" /home/ashish/.local/ |
+    $mdiff_cmd "$dotfiles/locals/" /home/ashish/.local/ |
         grep -Ev '^Only.*(local/(:)|(share(:)|(applications: (mimeapps\.list)|(mimeinfo\.cache))|(builds: dwm)))'
-    $diff_cmd "$dotfiles/scripts/" /home/ashish/.scripts/
+    $mdiff_cmd "$dotfiles/scripts/" /home/ashish/.scripts/
 )
-print
+print_mdiff
 
 echo -e "\e[1;32mpackage list\e[0m"
-diff1=$(
-    $diff_cmd "$dotfiles/configs/pacsoff.txt" <( pacman -Qqen | grep -Fxvf <( pacman -Qqg base-devel ) |
+sdiff1=$(
+    $sdiff_cmd "$dotfiles/configs/pacsoff.txt" <( pacman -Qqen | grep -Fxvf <( pacman -Qqg base-devel ) |
         grep -Ev '(^base$)|(^efibootmgr$)|(^grub$)|(^linux$)|(^linux-firmware$)|(^linux-lts$)' )
 )
-diff2=$(
-    $diff_cmd "$dotfiles/configs/pacsaur.txt" <( pacman -Qqem )
+sdiff2=$(
+    $sdiff_cmd "$dotfiles/configs/pacsaur.txt" <( pacman -Qqem )
 )
-if [[ -n $diff1 ]] ; then
-    echo "diff '--color=always' -r "$dotfiles/configs/pacsoff.txt" pacsoff.txt"
-    if [[ -n $diff2 ]] ; then
-        printf "%s\n" "$diff1"
-        echo "diff '--color=always' -r "$dotfiles/configs/pacsaur.txt" pacsaur.txt"
-        printf "%s\n\n" "$diff2"
-    else
-        printf "%s\n\n" "$diff1"
-    fi
-elif [[ -n $diff2 ]] ; then
-    echo "diff '--color=always' -r "$dotfiles/configs/pacsaur.txt" pacsaur.txt"
-    printf "%s\n\n" "$diff2"
+if [[ -n $sdiff1 && -n $sdiff2 ]] ; then
+    printf "$sdiff_str $dotfiles/configs/pacsoff.txt pacsoff\n%s\n$sdiff_str $dotfiles/configs/pacsaur.txt pacsaur\n%s\n\n" "$sdiff1" "$sdiff2"
+elif [[ -n $sdiff2 ]] ; then
+    printf "$sdiff_str $dotfiles/configs/pacsaur.txt pacsaur\n%s\n\n" "$sdiff2"
 else
     echo -e "No changes\n"
 fi
-
-echo "Manually check crontab for updates"
