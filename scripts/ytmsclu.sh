f8ee1c881cabc37469d2af6f80ba4fcaa9593305
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-05 16:24:20 +0530

    Improved ytm scripts

--- scripts/ytmsclu.sh
+++ scripts/ytmsclu.sh
@@ -4,12 +4,12 @@ xdotool keyup 108
 [[ $1 != 1 && $1 != 0 ]] && { echo "Usage: $0 1|0"; exit ;}
 
 hide_exit() {
-        [ -n "$ytaf" ] || xsetroot -name "z:scrh i 2"
+        [[ -n $ytaf ]] || xsetroot -name "z:scrh i 2"
         exit
 }
 
-if [ "$(focusedwinclass -i)" = crx_cinhimbnkkaeohfgghhklpknlkffjgod ] ; then
-    # YouTube Music container already focused
+if [[ $(focusedwinclass -i) == crx_cinhimbnkkaeohfgghhklpknlkffjgod ]] ; then
+    # YouTube Music window already focused
     ytaf=1
 else
     if xwininfo -tree -root | grep -q '("crx_cinhimbnkkaeohfgghhklpknlkffjgod" ' ; then
@@ -20,22 +20,22 @@ else
     fi
 fi
 
-# exit if the focused container doesn't contain YouTube Music window
+# exit if the focused window doesn't have YouTube Music at the end of its title
 [[ $(xdotool getactivewindow getwindowname) =~ "YouTube Music"$ ]] || exit
 
 geometry=$(xdotool getactivewindow getwindowgeometry)
 position=${geometry##*Position: }
 size=${position##*Geometry: }
-# coordinates of right upper corner of the main YouTube Music window
+# coordinates of right upper corner of the YouTube Music window
 x0=${position%%,*}
 y0=${position##*,}; y0=${y0%% (screen: *}
-# size of the main YouTube Music window
+# size of the YouTube Music window
 x=${size%%x*}
 y=${size##*x}
 
-# coordinates of the black pixel with coordinates (9,40) wrt left lower corner
-Xb=$(( x0 + 9 ))
-Yb=$(( y0 + y - 40 ))
+# coordinates of the white pixel at the center of the youtube music icon in the top left corner
+Xw=$(( x0 + 32 ))
+Yw=$(( y0 + 59 ))
 # size of the pixel array to capture and analyze; 500 cut from left side and 300 from right
 Xs=$(( x - 800 ))
 # coordinates of the pixel which will be the leftmost point of the pixel array to be captured later
@@ -47,17 +47,16 @@ Y0=$(( y0 + y - 35 ))
 if (( x < 944 || y < 65 )) ; then
     notify-send -t 3000 "YouTube Music L/U Script" \
         "The size of the YouTube Music window is less than can be tolerated by the script."
-    hide_exit
+    exit
 fi
-if (( Xb < 0 || Xb > 1365 || Yb < 0 || Yb > 767 || X0 < 0 || (X0 + Xs) > 1365 || Y0 < 0 || Y0 > 767 )) ; then
+if (( Xw < 0 || Xw > 1365 || Yw < 0 || Yw > 767 || X0 < 0 || (X0 + Xs) > 1365 || Y0 < 0 || Y0 > 767 )) ; then
     notify-send -t 4000 "YouTube Music L/U Script" \
         "The position of the YouTube Music window is problematic. Some essential window parts are offscreen."
-    hide_exit
+    exit
 fi
-
-if [[ ! $(import -window root -depth 8 -crop "1x1+${Xb}+${Yb}" txt:- | grep -om1 '#\w\+') == "#212121" ]] ; then
+if [[ $(import -window root -depth 8 -crop "1x1+${Xw}+${Yw}" txt:- | grep -om1 '#\w\+') != "#FFFFFF" ]] ; then
     notify-send -u critical -t 0 "YouTube Music L/U Script" "Something is wrong!"
-    hide_exit
+    exit
 fi
 
 if [[ $1 == 1 ]] ; then
@@ -71,7 +70,6 @@ if [[ $1 == 1 ]] ; then
               ${ytmglst[$(( i + 3 ))]} == "$(( ${ytmglst[$i]} + 3 ))" &&
               ${ytmglst[$(( i + 4 ))]} == "$(( ${ytmglst[$i]} + 6 ))" &&
               ${ytmglst[$(( i + 5 ))]} == "$(( ${ytmglst[$i]} + 7 ))" ]] ; then
-
             xdotool key plus
             hide_exit
         fi
@@ -84,17 +82,17 @@ else
         awk -F'[, ]' '/#[D-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F]/ {print $1}' )
 
     for i in ${!ytmglst[*]} ; do
-        if [[ ${ytmglst[$(( i + 1 ))]} == "$(( ${ytmglst[$i]} + 1 ))" &&
-              ${ytmglst[$(( i + 2 ))]} == "$(( ${ytmglst[$i]} + 2 ))" &&
-              ${ytmglst[$(( i + 3 ))]} == "$(( ${ytmglst[$i]} + 3 ))" &&
-              ${ytmglst[$(( i + 4 ))]} == "$(( ${ytmglst[$i]} + 6 ))" &&
-              ${ytmglst[$(( i + 5 ))]} == "$(( ${ytmglst[$i]} + 7 ))" &&
-              ${ytmglst[$(( i + 8 ))]} == "$(( ${ytmglst[$i]} + 10 ))" &&
+        if [[ ${ytmglst[$(( i + 1 ))]}  == "$(( ${ytmglst[$i]} + 1 ))"  &&
+              ${ytmglst[$(( i + 2 ))]}  == "$(( ${ytmglst[$i]} + 2 ))"  &&
+              ${ytmglst[$(( i + 3 ))]}  == "$(( ${ytmglst[$i]} + 3 ))"  &&
+              ${ytmglst[$(( i + 4 ))]}  == "$(( ${ytmglst[$i]} + 6 ))"  &&
+              ${ytmglst[$(( i + 5 ))]}  == "$(( ${ytmglst[$i]} + 7 ))"  &&
+              ${ytmglst[$(( i + 8 ))]}  == "$(( ${ytmglst[$i]} + 10 ))" &&
               ${ytmglst[$(( i + 11 ))]} == "$(( ${ytmglst[$i]} + 13 ))" ]] ; then
-
             xdotool key plus
             hide_exit
         fi
     done
     hide_exit
+
 fi
--- scripts/ytresume.sh
+++ scripts/ytresume.sh
@@ -42,9 +42,8 @@ if [ "$x" -ge 944 ] && [ "$y" -ge 70 ] ; then
     if [ "$Xp" -lt 0 ] || [ "$Xp" -gt 1365 ] || [ "$Yp" -lt 0 ] || [ "$Yp" -gt 767 ] ; then
         notify-send -t 4000 "YouTube Music Resume Script" \
             "The position of the YouTube Music window is problematic. Some essential window parts are offscreen."
-        hide_exit
+        exit
     fi
-
     if [ "$(import -window root -depth 8 -crop "1x1+${Xp}+${Yp}" txt:- | grep -om1 '#\w\+')" = "#FFFFFF" ] ; then
         xdotool key space
         hide_exit
@@ -57,7 +56,7 @@ Yw=$(( y0 + 59 ))
 if [ "$Xw" -lt 0 ] || [ "$Xw" -gt 1365 ] || [ "$Yw" -lt 0 ] || [ "$Yw" -gt 767 ] ; then
     notify-send -t 4000 "YouTube Music Resume Script" \
         "The position of the YouTube Music window is problematic. Some essential window parts are offscreen."
-    hide_exit
+    exit
 fi
 
 case $(import -window root -depth 8 -crop "1x1+${Xw}+${Yw}" txt:- | grep -om1 '#\w\+') in
