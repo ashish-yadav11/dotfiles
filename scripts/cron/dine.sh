217fe1543ed4430d71f4da286c2b3ce10d591d29
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-12 18:34:04 +0530

    No need to update time in status from cron jobs now

--- scripts/cron/dine.sh
+++ scripts/cron/dine.sh
@@ -6,10 +6,8 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 trap 'rm -f /tmp/sleep_dine.pid; exit' HUP INT TERM
 echo $$ >/tmp/sleep_dine.pid
-/home/ashish/.local/bin/sigdsblocks 1 # update time in status
 notify-send -t 600000 "wrap up, it's time for dinner"
 sleep 600
-/home/ashish/.local/bin/sigdsblocks 1 # update time in status
 feh -FNY /home/ashish/Pictures/dine.png &
 sleep 20
 pkill -P $$
