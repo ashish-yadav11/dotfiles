f35a547d2e886a27ae3938959145bbc44c04aa0f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-12 13:49:17 +0530

    Updated dsblocks signals

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -1,7 +1,7 @@
 #!/bin/dash
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-/home/ashish/.local/bin/sigdsblocks 10 # update time in status
+/home/ashish/.local/bin/sigdsblocks 1 # update time in status
 date=$(date +%d)
 if [ "$date" -le 07 ] || { [ "$date" -ge 15 ] && [ "$date" -le 21 ] ;} ; then
     notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Timeshift log, Cache of uninstalled packages\n3) Reset atq\n4) Backup"
