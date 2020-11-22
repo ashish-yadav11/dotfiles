031ce5659953fce6bc31f94d37de05d7cdc73844
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-22 13:11:45 +0530

    Updated volume block scripts for dsblocks

--- scripts/pulse_daemon.sh
+++ scripts/pulse_daemon.sh
@@ -1,6 +1,7 @@
 #!/bin/dash
-
 volume_step=1
+headphone_step=1
+
 volume_handler() {
     case $volume_step in
         1)
@@ -31,7 +32,7 @@ volume_handler() {
         4)
             case $event in
                 "Event 'remove' on client #$client_num")
-                    sigdsblocks 2
+                    sigdsblocks 2 1
                     volume_step=1
                     return 0
                     ;;
@@ -42,17 +43,6 @@ volume_handler() {
     return 1
 }
 
-snd_card_num=1
-update_headphone() {
-    if grep -A3 'Headphone Playback Switch' /proc/asound/card$snd_card_num/codec#0 |
-           grep -qF 'Amp-Out vals:  [0x00 0x00]' ; then
-        sigdsblocks 2 1
-    else
-        sigdsblocks 2 0
-    fi
-}
-
-headphone_step=1
 headphone_handler() {
     case $headphone_step in
         1)
@@ -66,7 +56,7 @@ headphone_handler() {
         2)
             case $event in
                 "Event 'change' on source"*)
-                    update_headphone
+                    sigdsblocks 2 -1
                     headphone_step=1
                     return 0
                     ;;
@@ -77,7 +67,6 @@ headphone_handler() {
     return 1
 }
 
-update_headphone
 pactl subscribe |
     while read -r event ; do
         volume_handler || headphone_handler
