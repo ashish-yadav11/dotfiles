0baef871bc8eb52abd853ba58993bd87d51763a1
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-24 13:55:51 +0530

    Improved dictionary scripts

--- scripts/dictionary.sh
+++ scripts/dictionary.sh
@@ -1,15 +1,19 @@
 #!/bin/dash
+truncate() {
+    sed 's/^[[:space:][:punct:]]*//; s/[[:space:][:punct:]]*$//' | tr '[:upper:]' '[:lower:]'
+}
+
 case $1 in
     sel)
-        word=$(xclip -seletion primary -out | tr '[:upper:]' '[:lower:]' | xargs)
+        word=$(xclip -seletion primary -out | truncate)
         if [ -z "$word" ] ; then
             notify-send -t 2000 Dictionary "Nothing in primary selection"
             exit
         fi
         ;;
     *)
-        word=$(yad --title=Dictionary --image=dictionary --no-buttons --entry --text=Dictionary \
-                   --entry-label="Look up:" | tr '[:upper:]' '[:lower:]' | xargs)
+        word=$(yad --title=Dictionary --image=dictionary --no-buttons --entry \
+                   --text=Dictionary --entry-label="Look up:" | truncate)
         [ -z "$word" ] && exit
         ;;
 esac
@@ -19,10 +23,10 @@ case $? in
     0)
         logfile=/home/ashish/Documents/.dictionary.log
         mv /tmp/dictionary.last.temp /tmp/dictionary.last
-        termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         sed -e "1i\\$word" -e "/^$word$/d" "$logfile" |
             head -10000 >/tmp/dictionary.log.temp
         mv /tmp/dictionary.log.temp "$logfile"
+        termite --name=floating_Termite -t Dictionary -e "less /tmp/dictionary.last"
         ;;
     21)
         mv /tmp/dictionary.last.temp /tmp/dictionary.last
--- scripts/dictionary_history.sh
+++ scripts/dictionary_history.sh
@@ -4,10 +4,10 @@ log=$(cat "$logfile")
 if [ -n "$log" ] ; then
     if word=$(echo "$log" | fzf --sync) ; then
         dict "$word" >/tmp/dictionary.last
-        less /tmp/dictionary.last
         sed -e "1i\\$word" -e "/^$word$/d" "$logfile" |
             head -10000 >/tmp/dictionary.log.temp
         mv /tmp/dictionary.log.temp "$logfile"
+        less /tmp/dictionary.last
     fi
 else
     notify-send -t 2000 Dictionary "History log is empty"
