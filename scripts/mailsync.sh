0fa553092f1920d17f8803f9813222fbbfa67476
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-04 02:06:09 +0530

    Fixed typo; added mark new trash as seen - mailsync.sh

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -4,19 +4,23 @@ exec >/dev/null 2>&1
 sigdsblocks 3 1
 
 exec 8>/tmp/mailsync.1.lock
-# Exit if some other instance of the script is
-# running which hasn't crossed the pinging stage
+# Exit if some other instance of the script is running which hasn't
+# crossed the pinging stage
 flock -n 8 || exit
 
 exec 9>/tmp/mailsync.2.lock
-# Wait while some other instance of the script is
-# running which has crossed the pinging state
+# Wait while some other instance of the script is running which has
+# crossed the pinging stage
 flock 9 || exit
 
 if ping -c1 imap.gmail.com ; then
     exec 8>/dev/null
     sigdsblocks 3 -2
+    time=$(date +%s)
     if mbsync iiser ; then
+        trash_folder="/home/ashish/.local/share/mail/iiser/[Gmail].Trash/"
+
+        find "$trash_folder" -newerct "@$time" -type f | mflag -S
         sigdsblocks 3 3
     else
         sigdsblocks 3 4
