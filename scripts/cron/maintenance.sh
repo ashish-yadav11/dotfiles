f97be9a63f275e415b1d16e05a53dfd647b29356
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-05 19:03:42 +0530

    Updated maintenance.sh

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -3,7 +3,7 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 date=$(date +%d)
 if [ "$date" -le 07 ] || { [ "$date" -ge 15 ] && [ "$date" -le 21 ] ;} ; then
-    notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Reset atq\n4) Backup"
+    notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
 else
     notify-send -t 0 "Upgrade system"
 fi
