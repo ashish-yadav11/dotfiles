72f721e50040f5668cc66478b11711baa90a248d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-12 00:20:06 +0530

    Fixed a bug in killing sleep-dine

--- scripts/systemd/sleep_hook_pre.sh
+++ scripts/systemd/sleep_hook_pre.sh
@@ -2,5 +2,5 @@
 hotspot=/home/ashish/.scripts/hotspot.sh
 
 exec >/dev/null 2>&1
-read -r PID </tmp/sleep_dine.pid && /usr/bin/kill -- "-$PID"
+read -r PID </tmp/sleep_dine.pid && kill "$PID" $(pgrep -P "$PID")
 sudo $hotspot terminate
