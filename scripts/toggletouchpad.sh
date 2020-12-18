467f6322b4d1ce59182cecba7288e6ca7c28027d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-18 21:55:15 +0530

    Optimization

--- scripts/toggletouchpad.sh
+++ scripts/toggletouchpad.sh
@@ -2,7 +2,7 @@
 notify="notify-send -h string:x-canonical-private-synchronous:toggletouchpad"
 touchpad="ETPS/2 Elantech Touchpad"
 
-if xinput --list-props "$touchpad" | grep -q 'Device Enabled.*1$' ; then
+if xinput --list-props "$touchpad" | grep -qm1 'Device Enabled.*1$' ; then
     xinput disable "$touchpad" && $notify -t 1000 "Touchpad disabled"
 else
     xinput enable "$touchpad" && $notify -t 1000 "Touchpad enabled"
