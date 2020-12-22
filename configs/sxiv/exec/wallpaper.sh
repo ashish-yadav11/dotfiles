0454f57f0d05dc34ffb6a2aa16ec386926862461
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-23 01:16:04 +0530

    Use xwallpaper to set wallpaper

--- configs/sxiv/exec/wallpaper.sh
+++ configs/sxiv/exec/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/dash
-feh --no-fehbg --bg-scale "$1"
+xwallpaper --stretch "$1"
 rm -f /home/ashish/.config/wall*
 filename=$(basename "$1")
 cp "$1" "/home/ashish/.config/wall.${filename##*.}"
