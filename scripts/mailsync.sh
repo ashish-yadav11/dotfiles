add90fe717d515aefa9c429b45ba06bb4b339540
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-24 13:21:49 +0530

    Removed unnecessary ping - mailsync.sh

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -4,30 +4,22 @@ trash_folder="/home/ashish/.local/share/mail/iiser/[Gmail].Trash/"
 
 exec >/dev/null 2>&1
 
-sigdsblocks 3 1
-
+# At max two instances of the script are allowed to run simultaneously,
+# one syncing and one waiting for the other to finish syncing.
 exec 8>/tmp/mailsync.1.lock
-# Exit if some other instance of the script is running which hasn't
-# crossed the pinging stage
-flock -n 8 || exit
+if ! flock -n 8 ; then
+    exec 9>/tmp/mailsync.2.lock
+    flock -n 9 || exit
+    flock 8
+fi
 
-exec 9>/tmp/mailsync.2.lock
-# Wait while some other instance of the script is running which has
-# crossed the pinging stage
-flock 9 || exit
-
-if ping -c1 imap.gmail.com ; then
-    exec 8>/dev/null
-    sigdsblocks 3 -2
-    time=$(date +%s)
-    if mbsync "$mbsync_channel" ; then
-        find "$trash_folder" -newerct "@$time" -type f | mflag -S
-        sigdsblocks 3 3
-    else
-        sigdsblocks 3 4
-    fi
+sigdsblocks 3 0
+time=$(date +%s)
+if msbync "$mbsync_channel" ; then
+    find "$trash_folder" -newerct "@$time" -type f | mflag -S
     notmuch new
+    sigdsblocks 3 1
 else
-    exec 8>/dev/null
-    sigdsblocks 3 -5
+    notmuch new
+    sigdsblocks 3 2
 fi
