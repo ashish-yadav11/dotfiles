3d04e9a016165d71ccd2d671e26cb8c907337b1d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-27 01:45:45 +0530

    Improved systemctl_timeout.sh

--- scripts/systemctl_timeout.sh
+++ scripts/systemctl_timeout.sh
@@ -4,25 +4,25 @@ notify="notify-send -h string:x-canonical-private-synchronous:timeout"
 case $1 in
     restart)
         systemctl restart timeout.service
-        $notify -t 1000 systemctl "timeout service restarted"
+        $notify -t 1000 "timeout service restarted"
         ;;
     toggle)
-        if systemctl --quiet is-active timeout.service ; then
+        if systemctl -q is-active timeout.service ; then
             systemctl stop timeout.service
-            $notify -t 1000 systemctl "timeout service stopped"
+            $notify -t 1000 "timeout service stopped"
         else
             systemctl start timeout.service
-            $notify -t 1000 systemctl "timeout service started"
+            $notify -t 1000 "timeout service started"
         fi
         ;;
     status)
-        if systemctl --quiet is-active timeout.service ; then
-            $notify -t 1000 systemctl "timeout service is up"
+        if systemctl -q is-active timeout.service ; then
+            $notify -t 1000 "timeout service is up"
         else
-            $notify -t 1000 systemctl "timeout service is down"
+            $notify -t 1000 "timeout service is down"
         fi
         ;;
     *)
-        echo "Usage: $0 toggle|status"
+        echo "Usage: $0 restart|toggle|status"
         ;;
 esac
