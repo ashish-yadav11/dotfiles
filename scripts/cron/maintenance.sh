742126a2bb64eae83c3284e1fdb521a61f538ebb
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-27 01:54:59 +0530

    Improved dunst config

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -1,7 +1,9 @@
 #!/bin/dash
+notify="notify-send -h string:x-canonical-private-synchronous:maintenance"
+
 date=$(date +%d)
 if [ "$date" -le 07 ] || [ "$date" -ge 15 ] && [ "$date" -le 21 ] ; then
-    notify-send -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
+    $notify -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
 else
-    notify-send -t 0 "Upgrade system"
+    $notify -t 0 "Upgrade system"
 fi
