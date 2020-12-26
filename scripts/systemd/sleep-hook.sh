337f05595d7bece68f8520f059c66ba7ec06d2ea
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-26 17:42:16 +0530

    Added sleep-hook.sh from /usr/lib/systemd/system-sleep/ to scripts/systemd

---
+++ scripts/systemd/sleep-hook.sh
@@ -0,0 +1,27 @@
+#!/bin/dash
+exec >/dev/null 2>&1
+
+export DISPLAY=:0
+export XAUTHORITY=/home/ashish/.Xauthority
+
+case $1 in
+    pre)
+        hotspot=/home/ashish/.scripts/hotspot.sh
+
+        read -r PID </tmp/sleep_dine.pid && kill "$PID" $(pgrep -P "$PID")
+        $hotspot terminate
+        ;;
+    post)
+        sigdsblocks 1
+        sigdsblocks 2
+        sigdsblocks 5
+        sigdsblocks 6
+
+        time=$(date +%H%M)
+        if [ "$time" -ge 2200 ] || [ "$time" -lt 0600 ] ; then
+            redshift -PO 4500
+        else
+            redshift -x
+        fi
+        ;;
+esac
