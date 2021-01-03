93b37debd803778b2068a0fff0ba20427de231bb
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-03 05:44:58 +0530

    Improved neomutt config

--- configs/mutt/scripts/import_in_calcurse.sh
+++ configs/mutt/scripts/import_in_calcurse.sh
@@ -3,6 +3,7 @@ aptsfile=/home/ashish/.local/share/calcurse/apts
 
 read -r -p "Import event(s) in calcurse? [Y/n]: " confirm
 [ "$confirm" = n ] || [ "$confirm" = N ] && exit
-calcurse -i "$1" >/dev/null 2>&1
-awk '!x[$0] {x[$0]=1; print}' "$aptsfile" >/tmp/apts.temp
-mv /tmp/apts.temp "$aptsfile"
+if calcurse -i "$1" >/dev/null 2>&1 ; then
+    awk '!x[$0] {x[$0]=1; print}' "$aptsfile" >/tmp/apts.temp
+    mv /tmp/apts.temp "$aptsfile"
+fi
--- configs/mutt/scripts/url_highlight_less.sh
+++ configs/mutt/scripts/url_highlight_less.sh
@@ -1,2 +1,2 @@
 #!/bin/dash
-grep --color=always -E -e '^' -e '\bhttps?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]' "$1" | less -R
+grep --color=always -E -e '^' -e '\bhttps?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]' | less -R
