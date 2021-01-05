949c8892602530f3886c30de47ff6e72f5552f95
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-06 02:13:16 +0530

    Clean - sleep-hook takes care of sleeping after script start

--- scripts/cron/dine.sh
+++ scripts/cron/dine.sh
@@ -9,11 +9,6 @@ id=$(dunstify -p -t 0 "wrap up, it's time for dinner")
 sleep 600
 dunstify -C "$id"
 id=""
-time=$(date +%H%M)
-if [ "$time" -lt 2059 ] || [ "$time" -gt 2101 ] ; then
-    rm -f /tmp/sleep_dine.pid
-    exit
-fi
 feh -FNY /home/ashish/Pictures/dine.png &
 sleep 20
 pkill -P "$$"
--- scripts/cron/sleep.sh
+++ scripts/cron/sleep.sh
@@ -9,11 +9,6 @@ id=$(dunstify -p -t 0 "wrap up, it's time to go to bed")
 sleep 600
 dunstify -C "$id"
 id=""
-time=$(date +%H%M)
-if [ "$time" -lt 0129 ] || [ "$time" -gt 0131 ] ; then
-    rm -f /tmp/sleep_dine.pid
-    exit
-fi
 feh -FNY /home/ashish/Pictures/sleep.jpg &
 sleep 20
 pkill -P "$$"
