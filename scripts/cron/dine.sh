8dbb4a6c3dd718cf81818b22bd5c6e1c8d8f6e53
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-07 02:07:24 +0530

    Presumably cronie doesn't run missed crontab jobs

--- scripts/cron/dine.sh
+++ scripts/cron/dine.sh
@@ -1,8 +1,4 @@
 #!/bin/dash
-time=$(date +%H%M)
-if [ "$time" -lt 2049 ] || [ "$time" -gt 2051 ] ; then
-    exit
-fi
 trap '[ -n "$id" ] && dunstify -C "$id"; rm -f /tmp/sleep_dine.pid; exit' TERM
 echo "$$" >/tmp/sleep_dine.pid
 id=$(dunstify -p -t 0 "wrap up, it's time for dinner")
--- scripts/cron/sleep.sh
+++ scripts/cron/sleep.sh
@@ -1,8 +1,4 @@
 #!/bin/dash
-time=$(date +%H%M)
-if [ "$time" -lt 0119 ] || [ "$time" -gt 0121 ] ; then
-    exit
-fi
 trap '[ -n "$id" ] && dunstify -C "$id"; rm -f /tmp/sleep_dine.pid; exit' TERM
 echo "$$" >/tmp/sleep_dine.pid
 id=$(dunstify -p -t 0 "wrap up, it's time to go to bed")
