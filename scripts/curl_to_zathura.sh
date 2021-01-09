ea99b5366ad2a07763c4fad0baf1d713c2a13047
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-09 15:40:47 +0530

    Another script - curl_to_zathura.sh

---
+++ scripts/curl_to_zathura.sh
@@ -0,0 +1,20 @@
+#!/bin/dash
+dmenu="dmenu -i -matching fuzzy -no-custom"
+
+case $1 in
+    selection)
+        url=$(xsel -ob)
+        if [ -z "$url" ] ; then
+            notify-send -t 2000 "curl to zathura" "Nothing in clipboard"
+            exit
+        fi
+        ;;
+    *)
+        url=$(yad --title="curl to zathura" --image=zathura --no-buttons --entry \
+                  --text="curl URL to zathura" --entry-label="URL:")
+        [ -z "$url" ] && exit
+        ;;
+esac
+case $(echo "Yes\nNo" | $dmenu -p "curl $url to zathura?") in
+    Yes) curl -s "$url" | zathura - ;;
+esac
