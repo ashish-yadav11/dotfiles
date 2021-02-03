2ff72dc291caffe9a1a976d3b5b27e27d7ea69bd
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-02-03 18:52:30 +0530

    Added pulse_normalize.sh to scripts

---
+++ scripts/pulse_normalize.sh
@@ -0,0 +1,26 @@
+#!/bin/dash
+volume=$(
+    pacmd list-sinks | awk '
+        {
+            if (f) {
+                if ($1 == "index:") {
+                    exit
+                }
+                if ($1 == "volume:") {
+                    v = $3 <= $10 ? $5 : $12
+                    gsub(/%/, "", v)
+                    v = int(v / 5) * 5
+                }
+            } else if ($1 == "*" && $2 == "index:") {
+                f = 1
+            }
+        }
+        END {
+            if (f) {
+                print v
+            } else {
+                exit 1
+            }
+        }
+    '
+) && pactl set-sink-volume @DEFAULT_SINK@ "$volume%"
