9a83e57953724584e9dc6b5cee8ff6396e8aad33
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-02-16 17:43:36 +0530

    Improved dictionary_history.sh

--- scripts/dictionary_history.sh
+++ scripts/dictionary_history.sh
@@ -1,9 +1,8 @@
 #!/bin/dash
 logfile=/home/ashish/.cache/dictionary.log
 
-log=$(cat "$logfile")
-if [ -n "$log" ] ; then
-    if word=$(echo "$log" | fzf --sync) ; then
+if [ -s "$logfile" ] ; then
+    if word=$(fzf --sync <"$logfile") ; then
         dict "$word" >/tmp/dictionary.last
         awk -v word="$word" '
             BEGIN {print word; a = 1}
