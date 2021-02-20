45c625449264c46f09e820b2f5bf75bfffec014c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-02-20 15:17:53 +0530

    Updated cron/maintenance.sh

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -2,7 +2,7 @@
 notify="notify-send -h string:x-canonical-private-synchronous:maintenance"
 
 date=$(date +%d)
-if [ "$date" -le 07 ] || [ "$date" -ge 15 ] && [ "$date" -le 21 ] ; then
+if [ "$date" -le 07 ] ; then
     $notify -t 0 "Time for system maintenance" "1) Upgrade system\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
 else
     $notify -t 0 "Upgrade system"
