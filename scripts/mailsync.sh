01476a8d2b687d853d9bb981fe3187169c27f806
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-03-01 22:46:14 +0530

    Simplified and Improved mailsync.sh

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -1,23 +1,18 @@
 #!/bin/dash
 mbsync_channel=iiser
-trash_folder="/home/ashish/.local/share/mail/iiser/[Gmail].Trash/"
+trash_folder="/home/ashish/.local/share/mail/iiser/[Gmail].Trash"
 
 # At max two instances of the script are allowed to run simultaneously,
 # one syncing and one waiting for the other to finish syncing.
 
-sigdsblocks 3 0
+sigdsblocks 3 -1
 exec >/dev/null 2>&1 8>/tmp/mailsync.1.lock 9>/tmp/mailsync.2.lock
 if ! flock -n 8 ; then
     flock -n 9 || exit
     flock 8
 fi
-time=$(date +%s)
-if mbsync "$mbsync_channel" ; then
-    find "$trash_folder" -newerct "@$time" -type f | mflag -S
-    sigval=1
-else
-    sigval=2
-fi
-exec 8>/dev/null
+mbsync "$mbsync_channel"
+sigval=$?
+find "$trash_folder"/* -type f | mflag -S
 notmuch new
 flock -n 9 && sigdsblocks 3 "$sigval"
