29457ca48cd8bc25b834c4490ddff39948d26133
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-03-13 21:32:38 +0530

    Fixed bugs in pomodoro.sh

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -4,7 +4,7 @@ notify="dunstify -h string:x-canonical-private-synchronous:pomodoro"
 if [ -n "$1" ] ; then
     case $1 in
         status)
-            if read -r PID </tmp/pomodoro.pid && kill -0 "$PID" ; then
+            if read -r PID 2>/dev/null </tmp/pomodoro.pid && kill -0 "$PID" ; then
                 $notify -t 1000 "pomodoro up"
             else
                 $notify -t 1000 "pomodoro down"
@@ -12,7 +12,7 @@ if [ -n "$1" ] ; then
             exit
             ;;
         stop)
-            read -r PID </tmp/pomodoro.pid && kill "$PID" $(pgrep -P "$PID")
+            read -r PID 2>/dev/null </tmp/pomodoro.pid && kill "$PID" $(pgrep -P "$PID")
             $notify -t 1000 "pomodoro terminated"
             exit
             ;;
@@ -29,13 +29,13 @@ else
 fi
 
 exec 9<>/tmp/pomodoro.pid
-flock -n 9 || read -r PID <&9 && kill "$PID" $(pgrep -P "$PID")
+flock -n 9 || { read -r PID 2>/dev/null <&9 && kill "$PID" $(pgrep -P "$PID") ;}
 flock -w1 9 || $notify -u critical -t 0 pomodoro "Something went wrong!"
 trap '
-    read -r NID </tmp/pomodoro.nid && dunstify -C "$NID"
+    read -r NID 2>/dev/null </tmp/pomodoro.nid && dunstify -C "$NID"
     flock -u 9 && flock -n 9 && rm -f /tmp/pomodoro.nid /tmp/pomodoro.pid
     exit
-' TERM
+' HUP INT TERM
 echo "$$" >/tmp/pomodoro.pid
 
 blocknotify() {
