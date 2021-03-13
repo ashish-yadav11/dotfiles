7a0d23bc015fa5dd3d9d602cd9d3ed204bb73b1d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-03-14 02:53:40 +0530

    Updated calcurse config; reverted notification setup

--- scripts/caclurse_notify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/dash
-output=$(calcurse -n); output=${output#*\[}
-h=${output%%:*}; output=${output#*:}
-m=${output%%\]*}
-title=${output#*\] }
-case $output in [IMP][DAH][CTY]\ [1-4][1-9][12]\ -\ slot) exit ;; esac
-t=$(( h * 60 + m ))
-notify-send -t "$(( t * 60000 ))" Calcurse "Appointment in $t minute(s)\n$title"
