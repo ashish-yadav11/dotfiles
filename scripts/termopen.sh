47e0ddfd7eb06243a60388e56b18faa5ec08638f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-05-23 01:07:04 +0530

    Another workaround for bug in xdg-open

--- scripts/termopen.sh
+++ scripts/termopen.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for arg in "$@" ; do
+if [[ $1 == ranger ]] ; then
+    command="ranger --cmd='set show_hidden=false'"
+else
+    command=$(printf %q "$1")
+fi
+for arg in "${@:2}" ; do
     command="$command $(printf %q "$arg")"
 done
-exec termite -e "${command# }"
+exec termite -e "$command"
