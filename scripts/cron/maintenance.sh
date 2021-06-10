eb8c57bfd0571af7fdebc942680528f319948564
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-06-10 14:14:53 +0530

    Backup bimonthly

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -1,8 +1,10 @@
 #!/bin/dash
 notify="notify-send -h string:x-canonical-private-synchronous:maintenance"
 
-date=$(date +%d)
-if [ "$date" -le 07 ] ; then
+dm=$(date +%d%m)
+date=${dm%??}
+month=${dm#??}
+if [ "$date" -le 07 ] && [ "$(( month % 2 ))" -eq 0 ] ; then
     $notify -t 0 "Time for system maintenance" "1) Upgrade — packages, vim plugins, zsh plugins, suckless stuff\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
 else
     $notify -t 0 "Upgrade system"
