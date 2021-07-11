abeaa704649b0e37bb76d9481d1f0079a6513b5c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-07-11 23:42:11 +0530

    Improved sxiv - added -- before file argument

--- config/sxiv/exec/wallpaper.sh
+++ config/sxiv/exec/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xwallpaper --stretch "$1"
+xwallpaper --stretch -- "$1"
 rm -f /home/ashish/.config/wall*
 filename=$(basename "$1")
-cp "$1" "/home/ashish/.config/wall.${filename##*.}"
+cp -- "$1" "/home/ashish/.config/wall.${filename##*.}"
