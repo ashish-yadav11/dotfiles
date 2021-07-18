20d0d5ea263cdfcc2dd28a5a32c999bfdfec7cfd
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-07-18 15:11:14 +0530

    Improved adb scripts

--- scripts/adb_shortcuts.sh
+++ scripts/adb_shortcuts.sh
@@ -1,5 +1,6 @@
 #!/bin/dash
 menu="rofi -dmenu -location 1 -width 100 -lines 1 -columns 9 -i -matching fuzzy -multi-select -no-custom"
+mtpmount=/home/ashish/.scripts/mtpmount.sh
 
 if [ "$(adb get-state)" != device ] ; then
     notify-send -t 2000 " adb" "Device not connected"
@@ -11,25 +12,16 @@ mtp="Media Transfer"
 rndis="USB Tethering"
 
 case $(adb shell getprop sys.usb.state) in
-    *rndis*) items="$mtp\n$none" ;;
-    *mtp*) items="$rndis\n$none" ;;
-    *) items="$rndis\n$mtp" ;;
+    *rndis*) current=rndis; items="$mtp\n$none" ;;
+    *mtp*) current=mtp; items="$rndis\n$none" ;;
+    *) current=none; items="$rndis\n$mtp" ;;
 esac
-
 case $(echo "$items" | $menu -p Select) in
-    "$mtp")
-        function=mtp
-        ;;
-    "$rndis")
-        function=rndis
-        ;;
-    "$none")
-        function=none
-        ;;
-    *)
-        exit
-        ;;
+    "$mtp") function=mtp ;;
+    "$rndis") function=rndis ;;
+    "$none") function=none ;;
+    *) exit ;;
 esac
-
 adb shell svc usb setFunctions "$function"
-:> /tmp/usbtether.lock
+echo -n "$current" >/tmp/usbtether.lock
+[ "$function" = mtp ] && timeout 10 adb wait-for-usb-device && $mtpmount
--- scripts/systemd/usbtether.sh
+++ scripts/systemd/usbtether.sh
@@ -1,3 +1,10 @@
 #!/bin/dash
-[ -f /tmp/usbtether.lock ] && { rm -f /tmp/usbtether.lock; exit ;}
-adb wait-for-usb-device shell svc usb setFunctions rndis
+mtpclean=/home/ashish/.scripts/mtpclean.sh
+
+if [ -f /tmp/usbtether.lock ] ; then
+    read -r last </tmp/usbtether.lock
+    rm -f /tmp/usbtether.lock
+    [ "$last" = mtp ] && $mtpclean
+else
+    timeout 10 adb wait-for-usb-device shell svc usb setFunctions rndis
+fi
