5cc45d74417c124ac029ddbf7d62d14596dfcfda
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-10 15:59:45 +0530

    Improved st/pager.sh

--- scripts/st/pager.sh
+++ scripts/st/pager.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 tmpfile="$(mktemp /tmp/st-XXXXXX)"
 cat >"$tmpfile"
-setsid -f st -e dash -c "nvim -c 'normal G' -R $tmpfile; rm -f $tmpfile"
+exec st -e sh -c "nvim -c 'normal G' -R '$tmpfile'; rm -f '$tmpfile'"
