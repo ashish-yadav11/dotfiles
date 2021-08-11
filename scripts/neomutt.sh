25d3717088b5cd209863689a082d3633addd4ac5
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-11 22:06:28 +0530

    neomutt.sh - only set name when asked for

--- scripts/neomutt.sh
+++ scripts/neomutt.sh
@@ -1,3 +1,5 @@
 #!/bin/dash
-st -n neomutt-st -e dash -c 'stty -ixon; neomutt'
+[ "$1" = scratch ] && options="-n neomutt-st"
+
+st $options -e dash -c 'stty -ixon; neomutt'
 pidof -sq /usr/bin/neomutt || rm -rf /tmp/neomutt/
