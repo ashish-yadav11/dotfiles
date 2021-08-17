963070ca0827fc3c5794fe433559ecc977e55767
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-18 01:21:10 +0530

    Updated diffab.sh

--- scripts/diffab.sh
+++ scripts/diffab.sh
@@ -53,7 +53,7 @@ mdiff="$(
             sed -e "s/^$mdiff_str/diff/"
 
     $mdiff_cmd "$dotfiles/local" /home/ashish/.local |
-        grep -Ev '^Only in .*(local(:|/share(:|/applications: mimeinfo\.cache$)|/builds(: dwm$|.*\.pkg\.tar\.zst$)))' |
+        grep -Ev '^Only in .*(local(:|/share(:|/applications: mimeinfo\.cache$)|/builds(: (dwm|st)$|.*\.pkg\.tar\.zst$)))' |
             sed -e "s/^$mdiff_str/diff/"
 
     $mdiff_cmd "$dotfiles/scripts" /home/ashish/.scripts |
