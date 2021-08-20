9157f539ffd65a17a4109c709dddfc8834ed9fc1
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-21 00:53:01 +0530

    less bind h to quit in mutt url_highlight; F1 in browser

--- config/mutt/scripts/url_highlight.sh
+++ config/mutt/scripts/url_highlight.sh
@@ -1,3 +1,4 @@
-#!/bin/dash
+#!/bin/bash
 # requires https://metacpan.org/pod/Regexp::Common::URI
-perl -M'Regexp::Common=URI' -pe 's/($RE{URI})/\e[1;31m$1\e[0m/g' | less -R
+perl -M'Regexp::Common=URI' -pe 's/($RE{URI})/\e[1;31m$1\e[0m/g' |
+    less --lesskey-src=<(echo 'h quit') -R
