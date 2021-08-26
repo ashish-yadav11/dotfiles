d63f9c6fade47257a82c781285d175ca65b96a0c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-26 23:47:04 +0530

    Improved compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,7 +1,13 @@
 #!/bin/dash
 file="$1"
 [ -f "$file" ] || { echo "Error: \"$file\" is not a valid file!"; exit ;}
+
 filename="${file%.*}"
+case "$filename" in
+    /*) execfile="$filename" ;;
+     *) execfile="./$filename" ;;
+esac
+
 fileext="${file##*.}"
 case "$fileext" in
     c) compiler=gcc ;;
@@ -11,15 +17,19 @@ esac
 
 errarg="-Wall -Wextra"
 optarg=""
+pedantic="-Wpedantic"
 case "$2" in *o*) optarg="-O3" ;; esac
 case "$2" in *E*) errarg="" ;; esac
-case "$2" in *r*) run=1 ;; esac
+case "$2" in *P*) pedantic="" ;; esac
+case "$2" in *r*)
+    case "$2" in
+        *f*) ;;
+        *) [ "$execfile" -nt "$file" ] 2>/dev/null && { "$execfile"; exit ;} ;;
+    esac
+    run=1
+    ;;
+esac
 
 shift "$(( $# < 2 ? $# : 2 ))"
-"$compiler" -o "$filename" $optarg $errarg "$@" "$file" || exit
-[ -n "$run" ] || exit
-
-case "$filename" in
-    /*) "$filename" ;;
-     *) "./$filename" ;;
-esac
+"$compiler" -o "$filename" $optarg $errarg $pedantic "$@" "$file" || exit
+[ -n "$run" ] && "$execfile"
