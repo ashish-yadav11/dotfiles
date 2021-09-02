71db5cf3d402dd133af0df3b82b48ec2bf17daca
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-09-02 16:01:31 +0530

    Improved maintenance.sh

--- scripts/cron/maintenance.sh
+++ scripts/cron/maintenance.sh
@@ -5,7 +5,7 @@ dm="$(date +%d%m)"
 date="${dm%??}"
 month="${dm#??}"
 if [ "$date" -le 07 ] && [ "$(( month % 2 ))" -eq 0 ] ; then
-    $notify -t 0 "Time for system maintenance" "1) Upgrade — packages, vim plugins, zsh plugins, suckless stuff\n2) Clean — Bleachbit, Cache of uninstalled packages\n3) Backup"
+    $notify -t 0 "Time for system maintenance" "1) Upgrade\n\npackages, vim plugins, zsh plugins, suckless stuff\n\n2) Clean\n\nconfig files, dconf settings, bleachbit, cache of uninstalled packages\n\n3) Backup"
 else
     $notify -t 0 "Upgrade system"
 fi
