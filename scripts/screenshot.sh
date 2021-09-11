d9060a38811dbb2180ca2e7c7483618afa52560b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-09-12 01:57:10 +0530

    Switched to maim and improved screenshot.sh

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,21 +1,47 @@
 #!/bin/dash
-menu="rofi -dmenu -location 1 -width 100 -lines 1 -columns 9 -i -matching fuzzy -no-custom"
+menu="rofi -dmenu -location 1 -width 100 -lines 1 -columns 9 -i -matching fuzzy"
 
-case "$(echo "Root\nFocused window\nSelection" | $menu -p Scrot)" in
-    Root)
-        sleep 0.100
-        ;;
-    "Focused window")
-        sleep 0.100
-        options="-u"
-        ;;
-    Selection)
-        options="-s"
-        ;;
-    *)
-        exit
-        ;;
+rt="Root"
+fw="Focused window"
+sl="Selection"
+selection="$(echo "$rt\n$fw\n$sl" | $menu -no-custom -p Maim)" || exit
+case "$selection" in
+    "$rt") options="-i root" ;;
+    "$fw") options="-i $(xdotool getactivewindow)" ;;
+    "$sl") options="-s -b 1 -p -1" ;;
+        *) exit ;;
 esac
 
-scrot -q75 $options '/home/ashish/Pictures/screenshots/%Y-%m-%d-%H%M%S_$wx$h.png' &&
-    notify-send -t 1000 Scrot "Screenshot captured"
+cn="Without cursor"
+cy="With cursor"
+selection="$(echo "$cn\n$cy" | $menu -no-custom -p Maim)" || exit
+case "$selection" in
+    "$cn") cursor="-u" ;;
+    "$cy") cursor="" ;;
+        *) exit ;;
+esac
+
+cb="Clipboard"
+dl="Default location"
+location="$(echo "$dl\n$cb" | $menu -p "Where to save the image?")" || exit
+[ -z "$location" ] && exit
+case "$location" in
+    "$dl") location="$HOME/Pictures/screenshots/$(date +%Y-%m-%d-%H%M%S).png" ;;
+    "$cb") clipboard=1 ;;
+    [!/]*) location="$HOME/$location" ;;
+esac
+if [ -n "$clipboard" ] ; then
+    { { { {
+        maim -q -f png -m 10 $cursor $options; echo "$?" >&8 ;} |
+            xclip -selection clipboard -t image/png >&9
+    } 8>&1 ;} | { read -r e; exit "$e" ;} ;} 9>&1 || exit
+else
+    dir="${location%/*}"
+    if ! [ -d "$dir" ] ; then
+        case "$dir" in "$HOME"*) dir="~${dir#"$HOME"}" ;; esac
+        notify-send -u critical Maim "$dir is not a directory"
+        exit
+    fi
+    maim -q -m 10 $cursor $options "$location" || exit
+fi
+notify-send -t 1000 Maim "Screenshot captured"
