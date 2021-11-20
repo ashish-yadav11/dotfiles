ca89de36aa3f3244f1845d73acdb6a19f1c6e3e6
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-11-20 16:09:25 +0530

    Updated sleepdine scripts

--- scripts/cron/sleepdine.sh
+++ scripts/cron/sleepdine.sh
@@ -2,14 +2,20 @@
 notify="dunstify -h string:x-canonical-private-synchronous:sleepdine"
 pidfile=/run/user/1000/sleep-dine.pid
 
+if [ "$1" = sleep ] ; then
+    notif="wrap up, it's time to go to bed"
+else
+    notif="wrap up, it's time for dinner"
+fi
+
 trap '[ -n "$id" ] && dunstify -C "$id"; rm -f "$pidfile"; exit' TERM
 echo "$$" >"$pidfile"
-id="$($notify -p -t 0 "wrap up, it's time for dinner")"
+id="$($notify -p -t 0 "$notif")"
 sleep 600
 dunstify -C "$id"
 id=""
-feh -FNY /home/ashish/Pictures/dine.png &
+feh -FNY "/home/ashish/Pictures/$1.jpg" &
 sleep 20
 pkill -P "$$"
 rm -f "$pidfile"
-systemctl hibernate
+systemctl suspend
