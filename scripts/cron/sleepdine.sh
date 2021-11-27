1de0be5d31ed910de5c58f62cfbd0bea0178c46b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-11-27 21:29:13 +0530

    Improved sleepdine.sh

--- scripts/cron/sleepdine.sh
+++ scripts/cron/sleepdine.sh
@@ -2,6 +2,12 @@
 notify="dunstify -h string:x-canonical-private-synchronous:sleepdine"
 pidfile=/run/user/1000/sleep-dine.pid
 
+if [ -z "$2" ] ; then
+    time="$(date +%H%M)"
+    if [ "$time" -gt "$(( $2 + 2 ))" || "$time" -lt "$(( $2 - 2 ))"  ] ; then
+        exit
+    fi
+fi
 if [ "$1" = sleep ] ; then
     notif="wrap up, it's time to go to bed"
 else
