d2404124f086fa55dfe477e3be8221db07c75a9d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-04-27 21:33:32 +0530

    Improved iiserlogin dispatcher script

--- scripts/networkmanager/01-iiserlogin.sh
+++ scripts/networkmanager/01-iiserlogin.sh
@@ -1,6 +1,14 @@
 #!/bin/dash
-[ "$1" = eno1 ] || exit
-case "$2" in
-    up) systemctl restart iiserlogin.service ;;
-    down) systemctl stop iiserlogin.service ;;
-esac
+interface="$1"
+status="$2"
+
+possibly_connected_to_iiser() {
+    [ "$interface" = eno1 ] ||
+        { [ "$interface" = wlp5s0 ] && [ "$CONNECTION_ID" = Students ] ;}
+}
+
+if [ "$status" = down ] ; then
+    possibly_connected_to_iiser && systemctl stop iiserlogin.service
+elif [ "$status" = up ] && [ "$DHCP4_DOMAIN_NAME" = "iiserpune.ac.in" ] ; then
+    possibly_connected_to_iiser && systemctl restart iiserlogin.service
+fi
