5169e596dc249404d35e8961b3b1c69c475011bd
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-07-28 16:23:44 +0530

    Improved reflector.sh

--- scripts/reflector_launch.sh
+++ scripts/reflector_launch.sh
@@ -1,6 +1,13 @@
 #!/bin/dash
+lockfile="$XDG_RUNTIME_DIR/reflector.lock"
 notify="notify-send -h string:x-canonical-private-synchronous:reflector"
 reflector=/home/ashish/.scripts/reflector.sh
+
+exec 9<>"$lockfile"
+if ! flock -n 9 ; then
+    $notify -u critical -t 2000 Reflector "Reflector is already running!"
+    exit
+fi
 
 $notify -t 1000 Reflector "Reflector initiated"
 if sudo $reflector ; then
