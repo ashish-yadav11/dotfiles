72fc9719df1be32e0c39b993a141f294ac2232c1
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-11-22 12:43:10 +0530

    Improved scrcpy.sh

--- scripts/scrcpy.sh
+++ scripts/scrcpy.sh
@@ -1,5 +1,17 @@
 #!/bin/dash
 ip=192.168.12.10
-{ ping -c1 -W1 "$ip" >/dev/null &&
-    scrcpy --tcpip="$ip" --max-size=1920 --shortcut-mod=lctrl ;} ||
-        notify-send -t 2000 " ScrCpy" "Device not connected"
+
+warn() {
+    notify-send -t 2000 " ScrCpy" "Device not connected"
+}
+checkoutput() {
+    grep -Fqm1 'WARN: Device disconnected' || warn
+}
+
+if ping -c1 -W1 "$ip" >/dev/null ; then
+    scrcpy --tcpip="$ip" --max-size=1920 --shortcut-mod=lctrl 2>&1 | checkoutput
+elif [ "$(adb devices | wc -l)" -ge 3 ] ; then
+    scrcpy -d --max-size=1920 --shortcut-mod=lctrl 2>&1 | checkoutput
+else
+    warn
+fi
