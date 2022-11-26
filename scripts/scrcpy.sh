a8bf5308def9e6fc42759aafd0393bc23e77e102
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-11-26 14:31:14 +0530

    scrcpy gives 1 when it can't find any device

--- scripts/scrcpy.sh
+++ scripts/scrcpy.sh
@@ -10,11 +10,10 @@ grepwarn() {
 
 if ping -c1 -W1 "$ip" >/dev/null ; then
     scrcpy --tcpip="$ip" --max-size=1920 --shortcut-mod=lctrl "$@"
-    # 0 normal, 2 disconnected, 141 sig exit
-    [ "$?" != 0 ] && [ "$?" != 2 ] && [ "$?" != 141 ] && warnnotif
+    [ "$?" == 1 ] && warnnotif
 elif [ "$(adb devices | wc -l)" -ge 3 ] ; then
     scrcpy -d --max-size=1920 --shortcut-mod=lctrl "$@"
-    [ "$?" != 0 ] && [ "$?" != 2 ] && [ "$?" != 141 ] && warnnotif
+    [ "$?" == 1 ] && warnnotif
 else
     warnnotif
 fi
