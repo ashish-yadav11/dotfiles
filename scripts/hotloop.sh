0d8688c5666b29765a192cc1334fa0ab01ccb049
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-02-16 04:02:44 +0530

    Improved hotloop.sh

--- scripts/hotloop.sh
+++ scripts/hotloop.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/bash
 hotspot="sudo /home/ashish/.scripts/hotspot.sh"
 hotspot_launch="/home/ashish/.scripts/hotspot_launch.sh"
 
@@ -8,32 +8,28 @@ coldef='\033[0m'
 
 empty_prompt() {
     clear
-    printf "${colnrm}Press [enter] to continue:${coldef} "
+    echo -ne "${colnrm}Press [enter] to continue:${coldef} "
 }
 
-case "$(tr '\0' '%' <"/proc/$PPID/cmdline")" in
-    "login -- $USER"%*)
-        :
-        ;;
-    *)
-        printf "${colwrn}Parent process of the program is not a login shell. Do you still want to continue?${coldef} [N/y]: "
-        read -r input
-        [ "$input" != y ] && [ "$input" != Y ] && exit
-        ;;
-esac
+if [[ ! "$(tr '\0' '%' <"/proc/$PPID/cmdline")" =~ "login -- $USER"%* ]] ; then
+    echo -ne "${colwrn}Parent process of the program is not a login shell. "
+    echo -ne "Do you still want to continue?${coldef} [N/y]: "
+    read -r input
+    [ "$input" != y ] && [ "$input" != Y ] && exit
+fi
 
 empty_prompt
 while read -r dummy ; do
-    if [ -n "$($hotspot list-running)" ] ; then
+    if [[ -n "$($hotspot list-running)" ]] ; then
         clear
-        printf "${colwrn}Hotspot is active, kill it?${coldef} [y/N]: "
-        read -r input
-        [ "$input" != y ] && [ "$input" != Y ] && { empty_prompt; continue ;}
+        echo -ne "${colwrn}Hotspot is active, kill it?${coldef} [y/N]: "
+        read -rt 5 input || { empty_prompt; continue ;}
+        [[ "$input" != y && "$input" != Y ]] && { empty_prompt; continue ;}
     else
         clear
-        printf "${colnrm}Hotspot is inactive, launch it?${coldef} [Y/n]: "
-        read -r input
-        { [ "$input" = n ] || [ "$input" = N ] ;} && { empty_prompt; continue ;}
+        echo -ne "${colnrm}Hotspot is inactive, launch it?${coldef} [Y/n]: "
+        read -rt 5 input || { empty_prompt; continue ;}
+        [[ "$input" = n || "$input" = N ]] && { empty_prompt; continue ;}
     fi
     clear
     $hotspot_launch &
