e2b432b27f88651f3d84b22652cb9bcc37a779ac
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-02-15 05:59:25 +0530

    Improved hotloop.sh

--- scripts/hotloop.sh
+++ scripts/hotloop.sh
@@ -2,33 +2,38 @@
 hotspot="sudo /home/ashish/.scripts/hotspot.sh"
 hotspot_launch="/home/ashish/.scripts/hotspot_launch.sh"
 
-colblu='\033[1;34m'
-colred='\033[1;31m'
+colnrm='\033[0;36m'
+colwrn='\033[1;31m'
 coldef='\033[0m'
+
+empty_prompt() {
+    clear
+    printf "${colnrm}Press enter to continue:${coldef} "
+}
 
 case "$(tr '\0' '%' <"/proc/$PPID/cmdline")" in
     "login -- $USER"%*)
         :
         ;;
     *)
-        printf "${colred}Parent process of the program is not a login shell. Do you still want to continue?${coldef} [N/y]: "
+        printf "${colwrn}Parent process of the program is not a login shell. Do you still want to continue?${coldef} [N/y]: "
         read -r input
         [ "$input" != y ] && [ "$input" != Y ] && exit
         ;;
 esac
-clear
 
+empty_prompt
 while read -r dummy ; do
     if [ -n "$($hotspot list-running)" ] ; then
         clear
-        printf "${colred}Hotspot is active, kill it?${coldef} [y/N]: "
+        printf "${colwrn}Hotspot is active, kill it?${coldef} [y/N]: "
         read -r input
-        [ "$input" != y ] && [ "$input" != Y ] && { clear; continue ;}
+        [ "$input" != y ] && [ "$input" != Y ] && { empty_prompt; continue ;}
     else
         clear
-        printf "${colblu}Hotspot is inactive, launch it?${coldef} [Y/n]: "
+        printf "${colnrm}Hotspot is inactive, launch it?${coldef} [Y/n]: "
         read -r input
-        { [ "$input" = n ] || [ "$input" = N ] ;} && { clear; continue ;}
+        { [ "$input" = n ] || [ "$input" = N ] ;} && { empty_prompt; continue ;}
     fi
     clear
     $hotspot_launch &
