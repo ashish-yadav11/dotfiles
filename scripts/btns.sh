a9b3ee3d8a1912404a87e14d921cc65694a25305
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-02-28 04:42:31 +0530

    Improved btns.sh

--- scripts/btns.sh
+++ scripts/btns.sh
@@ -2,21 +2,24 @@
 getbtnsfile=/sys/class/backlight/radeon_bl0/actual_brightness
 setbtnsfile=/sys/class/backlight/radeon_bl0/brightness
 
-checkint() {
-    if ! [ "$1" -eq "$1" ] 2>/dev/null ; then
+processval() {
+    val="$1"
+    case "$1" in *%) val="${1%\%}" ;; esac
+    if ! [ "$val" -eq "$val" ] 2>/dev/null ; then
         echo "Invalid argument!"
         exit
     fi
+    case "$1" in *%) val="$(( (255 * val) / 100 ))" ;; esac
 }
 
 incbtns() {
-    checkint "$inc"
+    processval "$1"
     read -r btns <"$getbtnsfile"
     if [ "$btns" -eq 255 ] ; then
         echo "Brightness is at its maximum value of 255"
         exit
     fi
-    btns="$(( btns + inc ))"
+    btns="$(( btns + val ))"
     if [ "$btns" -gt 255 ] ; then
         echo 255 >"$setbtnsfile"
         [ -z "$quiet" ] && echo 255/255
@@ -27,19 +30,28 @@ incbtns() {
 }
 
 decbtns() {
-    checkint "$dec"
+    processval "$1"
     read -r btns <"$getbtnsfile"
     if [ "$btns" -eq 1 ] ; then
         echo "Brightness is at its minimum value of 1"
         exit
     fi
-    btns="$(( btns - dec ))"
+    btns="$(( btns - val ))"
     if [ "$btns" -lt 1 ] ; then
         echo 1 >"$setbtnsfile"
         [ -z "$quiet" ] && echo "1/255"
     else
         echo "$btns" >"$setbtnsfile"
         [ -z "$quiet" ] && echo "$btns/255"
+    fi
+}
+
+setbtns() {
+    processval "$1"
+    if [ "$val" -ge 1 ] && [ "$val" -le 255 ] ; then
+        echo "$val" >"$setbtnsfile"
+    else
+        echo "Invalid argument!"
     fi
 }
 
@@ -50,28 +62,14 @@ else
     [ "$1" = -q ] && { quiet=1; shift ;}
     case "$1" in
         [+ip]*)
-            inc="${1#?}"
-            incbtns
-            ;;
+            incbtns "${1#?}" ;;
         [-dm]*)
-            dec="${1#?}"
-            decbtns
-            ;;
+            decbtns "${1#?}" ;;
         *[+ip])
-            inc="${1%?}"
-            incbtns
-            ;;
+            incbtns "${1%?}" ;;
         *[-dm])
-            dec="${1%?}"
-            decbtns
-            ;;
+            decbtns "${1%?}" ;;
         *)
-            checkint "$1"
-            if [ "$1" -ge 1 ] && [ "$1" -le 255 ] ; then
-                echo "$1" >"$setbtnsfile"
-            else
-                echo "Invalid argument!"
-            fi
-            ;;
+            setbtns "$1" ;;
     esac
 fi
