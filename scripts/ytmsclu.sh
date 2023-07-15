b525e7dcb15890a5dffa5eac830a3010bfc5659f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-07-15 08:08:43 +0530

    Improved ytmsclu.sh

--- scripts/ytmsclu.sh
+++ scripts/ytmsclu.sh
@@ -1,5 +1,6 @@
 #!/bin/dash
 lockfile="$XDG_RUNTIME_DIR/ytmsclu.lock"
+lk_lockfile="$XDG_RUNTIME_DIR/zsh_lk.lock"
 historyfile="/home/ashish/.config/BraveSoftware/Brave-Browser/Default/History"
 ytb_isliked="/home/ashish/.local/bin/ytb-isLiked"
 ytb_title="/home/ashish/.local/bin/ytb-title"
@@ -8,6 +9,7 @@ notify-send -t 500 ytmsclu "ytmsclu launched!"
 
 exec 9<>"$lockfile"
 flock 9
+exec 8<>"$lk_lockfile"
 
 unlockexit() {
     # xsel forks and takes the lock with it (see `man flock`)
@@ -57,12 +59,20 @@ fi
 $ytb_isliked "$url"
 case "$?" in
     0)
-        echo "Unlike" | menu -p "$title" &&
-                xdotool key --window "$winid" u l k Enter
+        echo "Unlike" | menu -p "$title" || unlockexit
+        while ! flock -n 8 ; do
+            echo "Unlike" | menu -p "(Scatch terminal busy!) $title" || unlockexit
+        done
+        flock -u 8
+        xdotool key --window "$winid" u l k l Enter
         ;;
     1)
-        echo "Like" | menu -p "$title" &&
-                xdotool key --window "$winid" d l k Enter
+        echo "Like" | menu -p "$title" || unlockexit
+        while ! flock -n 8 ; do
+            echo "Like" | menu -p "(Scatch terminal busy!) $title" || unlockexit
+        done
+        flock -u 8
+        xdotool key --window "$winid" d l k l Enter
         ;;
     *)
         notify-send -u critical -t 0 ytmsclu "Something went wrong with isliked script!"
