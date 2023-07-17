a6ac345baed20736a9213fa81d03c82802141063
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-07-17 20:21:20 +0530

    Fix + renaming

--- config/mpv/scripts/ytmhistory.lua
+++ config/mpv/scripts/ytmhistory.lua
@@ -1,8 +1,10 @@
 local musicdir = "/media/storage/Music"
 
+function fileloaded(event)
+    fname = mp.get_property("filename", "")
+end
 function endfile(event)
     if event["reason"] == "eof" then
-        local fname = mp.get_property("filename", "")
         os.execute("/home/ashish/.scripts/ytmsclu-history.sh '" .. fname:gsub("'", "'\\''") .. "'")
     end
 end
@@ -11,5 +13,6 @@ local path = mp.get_property("path", "")
 local wdir = mp.get_property("working-directory", "")
 local fullpath = wdir .. path
 if fullpath:sub(1, #musicdir) then
+    mp.register_event("file-loaded", fileloaded)
     mp.register_event("end-file", endfile)
 end
