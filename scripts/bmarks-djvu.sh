015e33f07c14a1f93da69a690cddd47f86810b51
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-07-29 03:42:08 +0530

    Improved bmarks-djvu.sh

--- scripts/bmarks-djvu.sh
+++ scripts/bmarks-djvu.sh
@@ -4,33 +4,52 @@
 awk '
     BEGIN {
         print "(bookmarks"
+        plevel = -1
+    }
+    /^            / {
+        closebracket(4, plevel)
+        printtitle(4)
+        plevel = 4
+        next
+    }
+    /^        / {
+        closebracket(3, plevel)
+        printtitle(3)
+        plevel = 3
+        next
     }
     /^    / {
-        flag = 2
-        printf "\n  (\"%s", $1
-        for (f=2; f<NF; f++) {
-            printf " %s", $f
-        }
-        printf "\"\n   \"#%s\" )", $NF
+        closebracket(2, plevel)
+        printtitle(2)
+        plevel = 2
         next
     }
     {
-        if (flag != 0) {
-            printf " )\n"
-        }
-        flag = 1
-        printf " (\"%s", $1
-        for (f=2; f<NF; f++) {
-            printf " %s", $f
-        }
-        printf "\"\n  \"#%s\"", $NF
+        closebracket(1, plevel)
+        printtitle(1)
+        plevel = 1
         next
     }
     END {
-        if (flag == 1) {
-            printf " ) )\n"
-        } else if (flag == 2) {
-            printf " ) ) )\n"
+        closebracket(0, plevel)
+    }
+
+    function printtitle(clevel) {
+        spc = sprintf("%*s", clevel, "")
+        printf "%s(\"%s", spc, $1
+        for (f=2; f<NF; f++) {
+            printf " %s", $f
+        }
+        printf "\"\n %s\"#%s\"", spc, $NF
+
+    }
+    function closebracket(clevel, plevel) {
+        i = plevel - clevel
+        while (i-- >= 0) {
+            printf " )"
+        }
+        if (i >= -2) {
+            printf "\n"
         }
     }
 ' "$1"
