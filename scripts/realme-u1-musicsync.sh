a21c29c922a8e6dced75a00d4e9c89b50fe54c4b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-08-03 17:21:43 +0530

    Improved musicsync

--- scripts/realme-u1-musicsync.sh
+++ scripts/realme-u1-musicsync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 src=/media/storage/Music/
 dst=( "$XDG_RUNTIME_DIR/mtp/RMX1831-9PLF7LKZKNFYLR5H-"*"/Internal shared storage/Music" )
-if [[ -d "${dst[0]}" ]] ; then
+testfile="${dst[0]}/0"
+if touch "$testfile" 2>/dev/null ; then
+    rm -f "$testfile"
     rsync -avu --delete  -f"- */" -f"+ *" "$src" "${dst[0]}"
 else
     echo "Destination device not mounted!"
