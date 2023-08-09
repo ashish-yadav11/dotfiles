57c62e48528fe24f5e21ea968d4637dc63b08757
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-08-09 09:45:08 +0530

    Automount realme on plugin

--- scripts/systemd/realme-u1.sh
+++ scripts/systemd/realme-u1.sh
@@ -1,6 +1,8 @@
 #!/bin/dash
+mount=/home/ashish/.scripts/realme-u1-mount.sh
 
 # to prevent udev rule from reverting manually choosen usb mode
 [ -f /tmp/realme-u1.lock ] && { rm -f /tmp/realme-u1.lock; exit ;}
 
+[ "$1" = mtp ] && setsid -f timeout 5 $mount
 adb -d wait-for-usb-device shell svc usb setFunctions "$1"
