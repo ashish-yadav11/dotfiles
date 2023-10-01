bf3bb236736fb2dcfe3ae7e3fe1c79dbce94640a
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-10-02 04:39:58 +0530

    Improved musicsync

--- scripts/musicsync.sh
+++ scripts/musicsync.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/dash
 src=/media/storage/Music/
-dst=( "$XDG_RUNTIME_DIR/mtp/"*"/Internal shared storage/Music" )
-testfile="${dst[0]}/0"
-if touch "$testfile" 2>/dev/null ; then
-    rm -f "$testfile"
-    rsync -avu --delete  -f"- */" -f"+ *" "$src" "${dst[0]}"
-else
-    echo "Destination device not mounted!"
-    exit 1
-fi
+for dst in "$XDG_RUNTIME_DIR/mtp/"*"/Internal shared storage/Music" ; do
+    testfile="$dst/0"
+    if touch "$testfile" 2>/dev/null ; then
+        rm -f "$testfile"
+        rsync -avu --delete  -f"- */" -f"+ *" "$src" "$dst"
+        exit 0
+    fi
+done
+echo "Destination device not mounted!"
+exit 1
