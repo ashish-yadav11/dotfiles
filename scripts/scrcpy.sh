0919cd5857e093e9c641774782218ea841aea4a1
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-10-25 18:36:13 +0530

    Improved scrcpy.sh

--- scripts/scrcpy.sh
+++ scripts/scrcpy.sh
@@ -1,13 +1,14 @@
 #!/bin/dash
 ip=192.168.12.10
 
-case "$(echo "Without Audio\nWith Audio" |
-            dmenu -p " ScrCpy" -i -matching fuzzy -no-custom)" in
-    "With Audio") scrcpy="scrcpy" ;;
-    "Without Audio") scrcpy="scrcpy --no-audio" ;;
-    *) exit ;;
-esac
-
+getaudiooption() {
+    case "$(echo "Without Audio\nWith Audio" |
+                dmenu -p " ScrCpy" -i -matching fuzzy -no-custom)" in
+        "With Audio") scrcpy="scrcpy" ;;
+        "Without Audio") scrcpy="scrcpy --no-audio" ;;
+        *) exit ;;
+    esac
+}
 warnnotif() {
     notify-send -t 2000 " ScrCpy" "Device not connected"
 }
@@ -16,12 +17,14 @@ grepwarn() {
 }
 
 if ping -c1 -W1 "$ip" >/dev/null ; then
+    getaudiooption
     $scrcpy --tcpip="$ip" --max-size=1920 --shortcut-mod=lctrl,rctrl "$@"
     if [ "$?" = 1 ] ; then
         $scrcpy --tcpip --max-size=1920 --shortcut-mod=lctrl,rctrl "$@"
         [ "$?" = 1 ] && warnnotif
     fi
 elif [ "$(adb devices | wc -l)" -gt 2 ] ; then
+    getaudiooption
     $scrcpy -d --max-size=1920 --shortcut-mod=lctrl,rctrl "$@"
     [ "$?" = 1 ] && warnnotif
 else
