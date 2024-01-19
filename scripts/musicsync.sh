1d1dcac46f36af1b65258aad517d19713d90461c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2024-01-19 06:52:52 +0530

    Fixed musicsync to work with samsung

--- scripts/musicsync.sh
+++ scripts/musicsync.sh
@@ -1,6 +1,9 @@
 #!/bin/dash
 src=/media/storage/Music/
-for dst in "$XDG_RUNTIME_DIR/mtp/"*"/Internal shared storage/Music" ; do
+for dst in "$XDG_RUNTIME_DIR/mtp/"*"/Internal shared storage/Music" "$XDG_RUNTIME_DIR/mtp/"*"/Internal storage/Music" ; do
+    case "$dst" in
+        "$XDG_RUNTIME_DIR/mtp/*/Internal shared storage/Music"|"$XDG_RUNTIME_DIR/mtp/*/Internal storage/Music") continue ;;
+    esac
     testfile="$dst/0"
     if touch "$testfile" 2>/dev/null ; then
         rm -f "$testfile"
