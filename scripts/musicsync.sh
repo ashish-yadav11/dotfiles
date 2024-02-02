c2064c1280e0c7aac97e673d2cfe1366bbd22c1c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2024-02-02 16:16:19 +0530

    Switched to better adbsync for music syncing

--- scripts/musicsync.sh
+++ scripts/musicsync.sh
@@ -1,15 +1,13 @@
 #!/bin/dash
 src=/media/storage/Music/
-for dst in "$XDG_RUNTIME_DIR/mtp/"*"/Internal shared storage/Music" "$XDG_RUNTIME_DIR/mtp/"*"/Internal storage/Music" ; do
-    case "$dst" in
-        "$XDG_RUNTIME_DIR/mtp/*/Internal shared storage/Music"|"$XDG_RUNTIME_DIR/mtp/*/Internal storage/Music") continue ;;
-    esac
-    testfile="$dst/0"
-    if touch "$testfile" 2>/dev/null ; then
-        rm -f "$testfile"
-        rsync -avu --delete  -f"- */" -f"+ *" "$src" "$dst"
-        exit 0
-    fi
-done
-echo "Destination device not mounted!"
-exit 1
+dst=/sdcard/Meuzik
+
+expected_device='List of devices attached
+ZD222HWT7V	device'
+
+if [ "$(adb devices)" != "$expected_device" ] ; then
+    echo "Unsupported (number of) adb devices!"
+    exit 1
+fi
+adbsync --adb-flag d --exclude 'archive' --exclude 'long' --del push "$src" "$dst" 2>&1 |
+    awk 'flag {print; next}; $0=="SYNCING" {flag=1; print "adbsync: starting sync"}'
