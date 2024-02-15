c821673ae9d2ed33a4485107310aacfa44f3881e
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2024-02-15 18:29:33 +0530

    Further simplification

--- scripts/scrcpy.sh
+++ scripts/scrcpy.sh
@@ -1,20 +1,11 @@
 #!/bin/dash
 
-getaudiooption() {
-    case "$(echo "Without Audio\nWith Audio" |
-                dmenu -p " ScrCpy" -i -matching fuzzy -no-custom)" in
-        "With Audio") scrcpy="scrcpy" ;;
-        "Without Audio") scrcpy="scrcpy --no-audio" ;;
-        *) exit ;;
-    esac
-}
 warnnotif() {
     notify-send -t 2000 " ScrCpy" "Device not connected"
 }
 
 if [ "$(adb devices | wc -l)" -gt 2 ] ; then
-    getaudiooption
-    $scrcpy -d --shortcut-mod=lctrl,rctrl "$@"
+    scrcpy --no-audio -d --shortcut-mod=lctrl,rctrl "$@"
     [ "$?" = 1 ] && warnnotif
 else
     warnnotif
