ee9aa6376ed973f36e0b1b09de87ee5e7b8c394d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-04-21 04:39:01 +0530

    Updated scripts

---
+++ scripts/dynscript.sh
@@ -0,0 +1,21 @@
+#!/bin/dash
+
+case "$1" in
+    1)
+        id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
+        output="$(xinput list-props "$id")" || exit
+        if echo "$output" | grep "Coordinate Transformation Matrix (149):	1" ; then
+            notify-send -t 1000 dwm 'Mouse movement disabled!'
+            xinput set-prop "$id" 149 0 0 0 0 0 0 0 0 1
+        else
+            notify-send -t 1000 dwm 'Mouse movement enabled!'
+            xinput set-prop "$id" 149 1 0 0 0 1 0 0 0 1
+        fi
+        ;;
+    2)
+        notify-send -t 0 dwm 'Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.\n\nIf we are not totally blind, what we are seeking is already here. This is it!\n\nSeeking what is true is not seeking what is desirable.'
+        ;;
+    3)
+        notify-send -t 0 dwm 'Hi!'
+        ;;
+esac
---
+++ scripts/systemd/mouse_udev.sh
@@ -0,0 +1,3 @@
+#!/bin/dash
+id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
+xinput set-prop "$id" 149 0 0 0 0 0 0 0 0 1
