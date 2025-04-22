e9432d85e56c09229a0d9fe857f9d95268e9d219
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-04-22 17:29:21 +0530

    toggle ytm on double prev

--- scripts/doubleprev.sh
+++ scripts/doubleprev.sh
@@ -1,2 +1,21 @@
 #!/bin/dash
-pactl set-sink-volume @DEFAULT_SINK@ -5%
+lckfile="/tmp/doubleprev.lck"
+sigfile="/tmp/doubleprev.sig"
+dt=0.3
+
+exec 9<>"$lckfile"
+
+if flock -n 9; then
+    rm -f "$sigfile"
+    sleep "$dt"
+    if [ -f "$sigfile" ] ; then
+        rm -f "$sigfile"
+    else
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
+    fi
+else
+    if ! [ -f "$sigfile" ] ; then
+        touch "$sigfile"
+        sigdwm "scrt i 2"
+    fi
+fi
