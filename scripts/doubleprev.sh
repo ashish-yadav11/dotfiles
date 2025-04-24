8c9c826206f3462283eb6cf5898a884da36ceedf
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-04-25 04:25:35 +0530

    triple to click, prev only double

--- scripts/doubleprev.sh
+++ scripts/doubleprev.sh
@@ -1,57 +1,32 @@
 #!/bin/dash
-lck7file="$XDG_RUNTIME_DIR/doubleprev.1.lck"
-lck8file="$XDG_RUNTIME_DIR/doubleprev.2.lck"
-lck9file="$XDG_RUNTIME_DIR/doubleprev.3.lck"
+lck1file="$XDG_RUNTIME_DIR/doubleprev.1.lck"
+lck2file="$XDG_RUNTIME_DIR/doubleprev.2.lck"
 dt=0.25
 ddt=0.003
 
-exec 7<>"$lck7file" 8<>"$lck8file" 9<>"$lck9file"
+exec 8<>"$lck1file" 9<>"$lck2file"
 
 action1() {
-    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    playerctl play-pause
 }
 action2() {
-    sigdwm "scrt i 2"
-}
-action3() {
-    playerctl previous
+    playerctl next
 }
 
 run1() {
     sleep "$dt"
-    if flock -n 8 ; then # we read-lock 8 to make this check foolproof
-        exec 9<&- 8<&- # order to make sure 8's free (for read-lock) after 9
+    if flock -n 8 ; then # we read-lock 8 first to make this check foolproof
+        exec 9<&- 8<&- # order to make sure 9 is free before 8 is free
         action1
     fi
 }
 run2() {
-    t0="$(date +%s%N)"
-    if ! flock -w1 9 ; then # wait for run1 to finish sleeping
-        notify-send -u critical -t 0 dwm 'doubleprev: something went wrong!'
-        exit
-    fi
-    exec 9<&-
-    sleep "$(echo "scale=3; ($dt*10^9 + $(date +%s%N) - $t0) / 10^9" | bc)"
-    if flock -n 7 ; then # we read-lock 7 first to make this check foolproof
-        exec 8<&- 7<&- # order to make sure 7's free (for read-lock) after 8
-        action2
-    fi
-}
-run3() {
-    action3 7<&- 8<&- 9<&-
-    if ! flock -w1 8 ; then # wait for run1 to finish sleeping
-        notify-send -u critical -t 0 dwm 'doubleprev: something went wrong!'
-        exit
-    fi
-    exec 8<&- 7<&- # order to make sure 7's free (for read-lock) after 8
+    action2 8<&- 9<&-
+    flock -w"$dt" 9 # wait for run1 to finish sleeping
+    exec 9<&- 8<&- # order to make sure 9 is free before 8 is free
 }
 
-# `-w"$ddt"`'s are required for very rare edge-cases...
-flock -w"$ddt" -s 7 || exit # exit if run3's running
-if flock -w"$ddt" -s 8 ; then # run2's not running
-    flock -n 9 && { exec 8<&- 7<&- 8<>"$lck8file"; run1; exit ;}
-    flock -w"$ddt" 8 && { exec 7<&- 7<>"$lck7file"; run2; exit ;}
-    flock -w"$ddt" 7 && { run3; exit ;}
-else # run2's running
-    flock -w"$ddt" 7 && { run3; exit ;}
-fi
+# `-w`'s are required for very rare edge-cases...
+flock -w"$ddt" -s 8 || exit
+flock -w"$ddt" 9 && { exec 8<&- 8<>"$lck1file"; run1; exit ;}
+flock -w"$ddt" 8 && { run2; exit ;}
