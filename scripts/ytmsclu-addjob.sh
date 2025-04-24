94bfdfe908c827e0b1e2b02854d536cd995ec91c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-04-24 20:10:01 +0530

    Improved addjob

--- scripts/ytmsclu-addjob.sh
+++ scripts/ytmsclu-addjob.sh
@@ -1,14 +1,14 @@
 #!/bin/dash
 lockfile="$XDG_RUNTIME_DIR/ytmsclu-daemon.lock"
 fifofile="$XDG_RUNTIME_DIR/ytmsclu-daemon.fifo"
-logfile="/home/ashish/.cache/ytmsclu-daemon.log"
+logfile="/home/ashish/.cache/ytmsclu-addjob.log"
 notifyerror="notify-send -u critical -t 0 ytmsclu-addjob"
 
 exec 9<>"$lockfile"
 
 if flock -n 9 || ! /usr/bin/echo "$*" 1<>"$fifofile" >"$fifofile" ; then
     $notifyerror "Error: ytmsclu-daemon not running!"
-    echo "$(date +%Y%m%d-%H%M%S) $*" >>"$logfile"
-    echo "Error: couldn't add job for ytmsclu-daemon\n\n" >>"$logfile"
-    exit
+    echo "-$(date +%Y%m%d-%H%M%S) $*" >>"$logfile"
+else
+    echo "+$(date +%Y%m%d-%H%M%S) $*" >>"$logfile"
 fi
