0e8ce06e9fe02d911165a67f2ae96377bbdd4c53
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-04-26 05:16:37 +0530

    I am going crazy...
    bound mouse clicks

--- scripts/systemd/mouse_udev.sh
+++ scripts/systemd/mouse_udev.sh
@@ -1,3 +1,4 @@
 #!/bin/dash
 id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
 xinput set-prop "$id" 149 0 0 0 0 0 0 0 0 1
+xinput set-button-map "$id" 91 92 93
