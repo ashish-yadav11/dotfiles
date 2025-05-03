e72a98d733868777d3a633e6d614d436ef66c998
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-05-04 04:02:22 +0530

    Fixed pulse_normalize

--- scripts/pulse_normalize.sh
+++ scripts/pulse_normalize.sh
@@ -1,18 +1,29 @@
-#!/bin/dash
-volume="$(
-    pactl list sinks | awk '
-        $1 == "Volume:" {
-            f=1
-            v = $3 <= $10 ? $5 : $12
-            sub(/%/, "", v)
-            v = int(v / 5 + 1/2) * 5
-            exit
+#!/bin/sh
+sink="$(pactl get-default-sink)"
+[ -n "$sink" ] || exit
+volume="$( 
+    pactl list sinks | awk -v sink="$sink" '
+        BEGIN {
+            step = 5
+        }
+        f {
+            if ($1 == "Volume:") {
+                v = $3 <= $10 ? $5 : $12
+                sub(/%/, "", v)
+                v = int(v / step + 1/2) * step
+                f = 2
+                exit
+            }
+            next
+        }
+        $1 == "Name:" && $2 == sink {
+            f = 1
         }
         END {
-            if (f)
+            if (f == 2)
                 print v
             else
                 exit 1
         }
     '
-)" && pactl set-sink-volume @DEFAULT_SINK@ "$volume%"
+)" && pactl set-sink-volume "$sink" "$volume%"
