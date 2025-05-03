4228f00ee67131b71a11fffd4a6cd377fc28b405
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-05-04 03:38:42 +0530

    Switch next-prev to the new implementation

--- scripts/doublenext.sh
+++ scripts/doublenext.sh
@@ -11,13 +11,13 @@ et=1 # >> t + dt + ddt
 exec 7<>"$lck7file" 8<>"$lck8file" 9<>"$lck9file"
 
 action1() {
-    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    pactl set-sink-volume @DEFAULT_SINK@ +1%
 }
 action2() {
     setsid -f /home/ashish/.scripts/ytmsclu.sh
 }
 action3() {
-    pactl set-sink-volume @DEFAULT_SINK@ +1%
+    pactl set-sink-volume @DEFAULT_SINK@ +3%
 }
 
 errorexit() {
@@ -41,7 +41,7 @@ run2() {
     sleep "$(echo "scale=3
         tl=(($t*10^9 - $(date +%s%N) + $t0) / 10^9)
         if (tl > 0) print tl else print 0" | bc)"
-    if flock -n 7 ; then # we read-lock 7 first to make this check foolproof
+    if flock -n 7 ; then
         exec 8<&- 7<&- # order to make sure 7's free (for read-lock) after 8
         action2
     else
@@ -49,17 +49,26 @@ run2() {
     fi
 }
 run3() {
+    t0="$(date +%s%N)"
     action3 7<&- 8<&- 9<&-
     flock -w"$et" 8 || errorexit # wait for run2 to finish sleeping
-    exec 8<&- 7<&- # order to make sure 7's free (for read-lock) after 8
+    exec 8<&-
+    sleep "$(echo "scale=3
+        tl=(($t*10^9 - $(date +%s%N) + $t0) / 10^9)
+        if (tl > 0) print tl else print 0" | bc)"
 }
 
 # `-w"$ddt"`'s are required for very rare edge-cases...
-flock -w"$ddt" -s 7 || exit # exit if run3's running
+if ! flock -w"$ddt" 7 ; then # run3's running
+    flock -w"$dt" -s 7 || errorexit
+    action1 7<&- 8<&- 9<&-
+    sleep "$t"
+    exit
+else
+    flock -s 7
+fi
 if flock -w"$ddt" -s 8 ; then # run2's not running
     flock -n 9 && { exec 8<&- 7<&- 8<>"$lck8file"; run1; exit ;}
     flock -w"$ddt" 8 && { exec 7<&- 7<>"$lck7file"; run2; exit ;}
-    flock -w"$ddt" 7 && run3
-else # run2's running
-    flock -w"$ddt" 7 && run3
 fi
+run3
