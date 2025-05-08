6909856c7a1a10ff42fc6dbf3890bd2fcad4f49c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-05-08 20:42:12 +0530

    Improved mouse_udev.sh

--- scripts/systemd/mouse_udev.sh
+++ scripts/systemd/mouse_udev.sh
@@ -1,4 +1,8 @@
 #!/bin/dash
 id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
-xinput set-prop "$id" 149 0 0 0 0 0 0 0 0 1
+
+prop="$(xinput list-props "$id" | grep -m1 'Coordinate Transformation Matrix')"
+prop="${prop#*"Matrix ("}"; prop="${prop%)*}"
+
+xinput set-prop "$id" "$prop" 0 0 0 0 0 0 0 0 1
 xinput set-button-map "$id" 91 92 93
