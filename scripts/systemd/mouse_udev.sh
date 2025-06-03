f367b39c18d5160cce9eb375b109fab3b70c1f96
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-06-03 06:03:34 +0530

    After boot udev triggers without X

--- scripts/systemd/mouse_udev.sh
+++ scripts/systemd/mouse_udev.sh
@@ -1,8 +1,10 @@
 #!/bin/dash
 id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
+[ -n "$id" ] || exit 0
 
 prop="$(xinput list-props "$id" | grep -m1 'Coordinate Transformation Matrix')"
 prop="${prop#*"Matrix ("}"; prop="${prop%)*}"
+[ -n "$prop" ] || exit 0
 
 xinput set-prop "$id" "$prop" 0 0 0 0 0 0 0 0 1
 xinput set-button-map "$id" 91 92 93
