6516cf08b3b46ad8b4cc5daec4d4e6323371c7e4
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2025-07-07 04:56:53 +0530

    Fixed dynscript.sh

--- scripts/dynscript.sh
+++ scripts/dynscript.sh
@@ -4,13 +4,19 @@ case "$1" in
     1)
         id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2); print $2}')"
         output="$(xinput list-props "$id")" || exit
-        if echo "$output" | grep "Coordinate Transformation Matrix (149):	1" ; then
-            notify-send -t 1000 dwm 'Mouse movement disabled!'
-            xinput set-prop "$id" 149 0 0 0 0 0 0 0 0 1
-        else
-            notify-send -t 1000 dwm 'Mouse movement enabled!'
-            xinput set-prop "$id" 149 1 0 0 0 1 0 0 0 1
-        fi
+        ctm="$(echo "$output" | grep -m1 'Coordinate Transformation Matrix')"
+        ctm="${ctm#*"Matrix ("}"
+        prop="${ctm%%)*}"
+        case "$ctm" in
+            "$prop):	1"*)
+                notify-send -t 1000 dwm 'Mouse movement disabled!'
+                xinput set-prop "$id" "$prop" 0 0 0 0 0 0 0 0 1
+                ;;
+            *)
+                notify-send -t 1000 dwm 'Mouse movement enabled!'
+                xinput set-prop "$id" "$prop" 1 0 0 0 1 0 0 0 1
+                ;;
+        esac
         ;;
     2)
         notify-send -t 0 dwm 'Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.\n\nIf we are not totally blind, what we are seeking is already here. This is it!\n\nSeeking what is true is not seeking what is desirable.'
--- scripts/systemd/mouse_udev.sh
+++ scripts/systemd/mouse_udev.sh
@@ -3,7 +3,7 @@ id="$(xinput list | awk -F'id=' '/PixArt USB Optical Mouse/ {gsub(/\t.*/, "", $2
 [ -n "$id" ] || exit 0
 
 prop="$(xinput list-props "$id" | grep -m1 'Coordinate Transformation Matrix')"
-prop="${prop#*"Matrix ("}"; prop="${prop%)*}"
+prop="${prop#*"Matrix ("}"; prop="${prop%%)*}"
 [ -n "$prop" ] || exit 0
 
 xinput set-prop "$id" "$prop" 0 0 0 0 0 0 0 0 1
