320f25909b18d2fa89b31a03a8962585d8b9fece
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2026-06-07 02:24:03 +0530

    Cleanup, don't login if already logged in

--- scripts/iiserlogin.sh
+++ scripts/iiserlogin.sh
@@ -1,21 +1,28 @@
 #!/bin/dash
 notify="notify-send -h string:x-canonical-private-synchronous:iiserlogin"
 
+GEN204URL="http://clients3.google.com/generate_204"
+case "$(curl -s -m1 -o /dev/null -w '%{http_code}' -L "$GEN204URL" || echo 000)" in
+    000)
+        $notify -t 2000 "Curl check for IISER captive portal failed!"
+        exit
+        ;;
+    204)
+        if [ "$1" != logout ] ; then
+            $notify -h int:transient:1 -t 2000 "Already logged in IISER captive portal!"
+            exit
+        fi
+        ;;
+esac
+
 username="$(pass captive-portal/username)"
 password="$(pass captive-portal/password)"
 
 PRODUCTTYPE=0
 USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100 Safari/537.36"
 
-COOKIEJAR="$(mktemp --tmpdir iiserlogin.XXXXXX)"
-cleanup() {
-    rm -f "$COOKIEJAR"
-    exit
-}
-trap cleanup HUP INT TERM
-
 sendloginrequest() {
-    curl -s -m 3 -c "$COOKIEJAR" -b "$COOKIEJAR" -A "$USERAGENT" -X POST \
+    curl -k -s -m 3 -A "$USERAGENT" -X POST \
         --url "https://firewall.iiserpune.ac.in:8090/login.xml" \
         --data-urlencode "mode=191" \
         --data-urlencode "username=$username" \
@@ -26,7 +33,7 @@ sendloginrequest() {
 
 sendliverequest() {
     lu=$(printf '%s' "$username" | tr '[:upper:]' '[:lower:]')
-    curl -s -m 3 -c "$COOKIEJAR" -b "$COOKIEJAR" -A "$USERAGENT" -G \
+    curl -k -s -m 3 -A "$USERAGENT" -G \
         --url "https://firewall.iiserpune.ac.in:8090/live" \
         --data-urlencode "mode=192" \
         --data-urlencode "username=$lu" \
@@ -34,9 +41,22 @@ sendliverequest() {
         --data-urlencode "producttype=$PRODUCTTYPE"
 }
 
+sendlogoutrequest() {
+    curl -k -s -m 3 -A "$USERAGENT" -X POST \
+        --url "https://firewall.iiserpune.ac.in:8090/logout.xml" \
+        --data-urlencode "mode=193" \
+        --data-urlencode "username=$username" \
+        --data-urlencode "a=$(date +%s)000" \
+        --data-urlencode "producttype=$PRODUCTTYPE"
+}
+if [ "$1" = logout ] ; then
+    sendlogoutrequest
+    exit
+fi
+
 notconnected() {
     $notify -h int:transient:1 -t 2000 "Not connected to IISER network"
-    cleanup
+    exit
 }
 
 loginsuccess() {
@@ -45,7 +65,7 @@ loginsuccess() {
 
 loginfailed() {
     $notify -t 4000 -u critical "Could not log into IISER captive portal"
-    cleanup
+    exit
 }
 
 output="$(sendloginrequest)" || notconnected
@@ -63,4 +83,3 @@ while true ; do
     printf '%s' "$output" | grep -qvF "Login failed" || loginfailed
     sleep 180
 done
-cleanup
