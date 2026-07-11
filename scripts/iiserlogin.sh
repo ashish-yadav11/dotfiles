568ad286f97014ec7fb99fe708a3997328cae6a7
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2026-07-12 02:02:59 +0530

    Made daemon mode more fullproof - iiserlogin

--- scripts/iiserlogin.sh
+++ scripts/iiserlogin.sh
@@ -1,18 +1,20 @@
 #!/bin/dash
 notify="notify-send -h string:x-canonical-private-synchronous:iiserlogin"
 
-livedt=180
-brutedt=3600
-
 username="$(pass captive-portal/username)"
 password="$(pass captive-portal/password)"
+
+captiveportalsite="https://firewall.iiserpune.ac.in:8090"
+liveinterval=180
+daemoninterval=7200
+pingsite="cloudflare.com"
 
 PRODUCTTYPE=0
 USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100 Safari/537.36"
 
 sendloginrequest() {
     curl -k -s -m 3 -A "$USERAGENT" -X POST \
-        --url "https://firewall.iiserpune.ac.in:8090/login.xml" \
+        --url "$captiveportalsite/login.xml" \
         --data-urlencode "mode=191" \
         --data-urlencode "username=$username" \
         --data-urlencode "password=$password" \
@@ -23,7 +25,7 @@ sendloginrequest() {
 sendliverequest() {
     lu=$(printf '%s' "$username" | tr '[:upper:]' '[:lower:]')
     curl -k -s -m 3 -A "$USERAGENT" -G \
-        --url "https://firewall.iiserpune.ac.in:8090/live" \
+        --url "$captiveportalsite/live" \
         --data-urlencode "mode=192" \
         --data-urlencode "username=$lu" \
         --data-urlencode "a=$(date +%s)000" \
@@ -32,7 +34,7 @@ sendliverequest() {
 
 sendlogoutrequest() {
     curl -k -s -m 3 -A "$USERAGENT" -X POST \
-        --url "https://firewall.iiserpune.ac.in:8090/logout.xml" \
+        --url "$captiveportalsite/logout.xml" \
         --data-urlencode "mode=193" \
         --data-urlencode "username=$username" \
         --data-urlencode "a=$(date +%s)000" \
@@ -57,6 +59,7 @@ loginfailed() {
     exit
 }
 
+sendlogoutrequest # to reset log out time on the server
 output="$(sendloginrequest)" || notconnected
 if printf '%s' "$output" | grep -qvF "Login failed" ; then
     loginsuccess
@@ -69,14 +72,30 @@ while true ; do
     output="$(sendliverequest)" || break
     if printf '%s' "$output" | grep -qFm1 "<ack><![CDATA[live_off]]></ack>" ; then
         [ "$1" != "daemon" ] && exit
+        iface="$(ip route show default)"
+        iface="${iface#*dev }"
+        iface="${iface%% proto*}"
         while true ; do
-            sleep "$brutedt"
+            sleep "$daemoninterval"
+            # wait for network inactivity for relogin, as it is an intrusive action
+            # also check if the system has been logged out just in case, using ping
+            lo=1
+            while true ; do
+                IFS='' read -r rx1 <"/sys/class/net/$iface/statistics/rx_bytes"
+                IFS='' read -r tx1 <"/sys/class/net/$iface/statistics/tx_bytes"
+                sleep 4
+                IFS='' read -r rx2 <"/sys/class/net/$iface/statistics/rx_bytes"
+                IFS='' read -r tx2 <"/sys/class/net/$iface/statistics/tx_bytes"
+                [ "$(( rx2 - rx1 + tx2 - tx1 ))" -lt 2000 ] && break
+                ping -q -c1 -W1 "$pingsite" >/dev/null 2>&1 || { lo=0; break ;}
+            done
+            [ "$lo" = 1 ] && sendlogoutrequest # to reset log out time on the server
             output="$(sendloginrequest)" || exit
             printf '%s' "$output" | grep -qvF "Login failed" || exit
         done
     fi
     if printf '%s' "$output" | grep -qFm1 "<ack><![CDATA[ack]]></ack>" ; then
-        sleep "$livedt"
+        sleep "$liveinterval"
         continue
     fi
     output="$(sendloginrequest)" || break
